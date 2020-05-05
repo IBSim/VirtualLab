b569ddc6d3d4905a0b2ebf4f4a58a655d1ad93c0
Llion Marc Evans <llion.evans@swansea.ac.uk>
2020-05-06 00:20:31 +0100

    Minor change

--- VLconfig.py
+++ VLconfig.py
@@ -5,7 +5,6 @@
 ### top directory.
 ### If left commented it is only possible to run from $VL_DIR
 #VL_DIR = "/PATH/TO/VirtualLab/HERE"
-VL_DIR = "/home/ubuntu/VirtualLab"
 
 ### PATH to various directories required as in/out for VirtualLab.
 ### If left commented default behaviour is to locate in $VL_DIR

173eea58663ad82a5ce1701b2cc73f2973f5530d
Llion Marc Evans <llion.evans@swansea.ac.uk>
2020-05-06 17:14:18 +0100

    Update to install scripts

--- Scripts/Install/Setup.sh
+++ Scripts/Install/Setup.sh
@@ -16,6 +16,12 @@ sudo -u ${SUDO_USER:-$USER} pip3 install numpy scipy matplotlib fpdf pillow h5py
 sudo -u ${SUDO_USER:-$USER} pip3 install iapws
 
 # Add $VL_DIR to $PYTHONPATH in python env and current shell
-sudo -u ${SUDO_USER:-$USER} echo 'export PYTHONPATH=$PYTHONPATH'$VL_DIR''  >> ~/.bashrc
-export PYTHONPATH=$PYTHONPATH$VL_DIR
+if grep -q PYTHONPATH='$PYTHONPATH'$VL_DIR ~/.bashrc; then
+  echo "Reference to VirtualLab PYTHONPATH found in .bashrc"
+else
+  echo "Adding $VL_DIR to PYTHONPATH"
+  sudo -u ${SUDO_USER:-$USER} echo 'export PYTHONPATH=$PYTHONPATH'$VL_DIR''  >> ~/.bashrc
+  export PYTHONPATH=$PYTHONPATH$VL_DIR
+fi
+
 

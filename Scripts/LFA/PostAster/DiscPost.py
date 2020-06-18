d51c96928a21eaf350069c384c0bc80cb16b9f45
Rhydian Lewis <930500@swansea.ac.uk>
2020-06-18 11:13:11 +0100

    Modified post processing script for LFA

--- Scripts/LFA/PostAster/DiscPost.py
+++ Scripts/LFA/PostAster/DiscPost.py
@@ -216,7 +216,6 @@ def Individual(Info, StudyDict):
 	print("Error: {}%\n".format(100*abs(1-ExactLaser/AprxLaser)))
 
 	StudyDict['ParaVis'] = ParaVis
-	print(StudyDict['ParaVis'])
 
 def Combined(Info):
 	GlobalRange = [np.inf, -np.inf]
@@ -224,133 +223,12 @@ def Combined(Info):
 	for Name, StudyDict in Info.Studies.items():
 		Simulations.append(Name)
 		StudyPV = StudyDict["ParaVis"]
-		print(StudyPV)
 		GlobalRange = [min(StudyPV["Range"][0],GlobalRange[0]), max(StudyPV["Range"][1],GlobalRange[1])]
+
 	PVDict = {"Simulations": Simulations, "GlobalRange":GlobalRange}
-
 	Info.WriteModule("{}/{}.py".format(Info.TMP_DIR, "PVParameters"), PVDict)
-	Info.SalomeRun(os.path.abspath(__file__), SalomeGUI=False, AddPath=Info.TMP_DIR)
 
-def ParaVis():
-	import sys
-	sys.dont_write_bytecode=True
-	import salome
-	import numpy as np
-	from importlib import import_module
-	import PVParameters
+	ParaVisFile = "{}/ParaVis.py".format(os.path.dirname(os.path.abspath(__file__)))
+	Info.SalomeRun(ParaVisFile, SalomeGUI=False, AddPath=Info.TMP_DIR)
 
-	import pvsimple
-	pvsimple.ShowParaviewView()
 
-	renderView1 = pvsimple.GetActiveViewOrCreate('RenderView')
-
-	for Sim in PVParameters.Simulations:
-		print(Sim)
-		Parameters = import_module("{}.Parameters".format(Sim))
-		PathVL = import_module("{}.PathVL".format(Sim))
-
-		thermalrmed = pvsimple.MEDReader(FileName="{}/{}.rmed".format(PathVL.ASTER,Parameters.ResName))
-
-		# show data in view
-		thermalrmedDisplay = pvsimple.Show(thermalrmed, renderView1)
-		thermalrmedDisplay.Representation = 'Surface'
-
-		# current camera placement for renderView1
-		camangle = 30
-		camradius = 0.025
-		renderView1.CameraPosition = [0, -camradius*np.cos(np.radians(camangle)), camradius*np.sin(np.radians(camangle))]
-		renderView1.CameraFocalPoint = [0,0,-0.002]
-		renderView1.CameraViewUp = [0,0,0]
-		renderView1.CameraParallelScale = 0.000
-		renderView1.Background = [1,1,1]  ### White Background
-
-		animationScene1 = pvsimple.GetAnimationScene()
-		animationScene1.AnimationTime = Parameters.CaptureTime
-		# animationScene1.UpdateAnimationUsingDataTimeSteps()
-
-		# set scalar coloring
-		pvsimple.ColorBy(thermalrmedDisplay, ('POINTS', 'resther_TEMP', 'TEMP'))
-
-		CBmin, CBmax = PVParameters.GlobalRange
-		resther_TEMPLUT = pvsimple.GetColorTransferFunction('resther_TEMP')
-		resther_TEMPLUT.NumberOfTableValues = 12
-		resther_TEMPLUT.RescaleTransferFunction(CBmin, CBmax)
-		resther_TEMPPWF = pvsimple.GetOpacityTransferFunction('resther_TEMP')
-		resther_TEMPPWF.RescaleTransferFunction(CBmin, CBmax)
-
-		ColorBar = pvsimple.GetScalarBar(resther_TEMPLUT, renderView1)
-		BarLength = 0.7
-		FontSize = 24
-		ColorBar.WindowLocation = 'AnyLocation'
-		#ColorBar.WindowLocation = 'LowerCenter'
-		ColorBar.Orientation = 'Horizontal'
-		ColorBar.ScalarBarLength = BarLength
-		ColorBar.ScalarBarThickness = FontSize
-		ColorBar.Position = [(1-BarLength)/2, 0.03]
-		ColorBar.Title = 'Temperature'
-		ColorBar.ComponentTitle = ''
-		ColorBar.TitleFontSize = FontSize
-		ColorBar.TitleColor = [0,0,0]
-		ColorBar.TitleBold = 1
-		ColorBar.LabelFontSize = FontSize
-		ColorBar.LabelColor = [0,0,0]
-		ColorBar.LabelBold = 1
-		ColorBar.CustomLabels = list(np.round(np.linspace(CBmin, CBmax,7), 2))
-		ColorBar.UseCustomLabels = 1
-		ColorBar.AddRangeLabels = 1
-		ColorBar.RangeLabelFormat = '%-#.2f'
-		ColorBar.Visibility = 1
-
-		pvsimple.SaveScreenshot("{}/Capture.png".format(PathVL.POSTASTER), renderView1, ImageResolution=[1400, 590], FontScaling='Do not scale fonts')
-		pvsimple.Hide(thermalrmed, renderView1)
-		print("Created image Capture.png")
-
-		clip1 = pvsimple.Clip(Input=thermalrmed)
-		clip1.ClipType.Normal = [0.0, -1.0, 0.0]
-		clip1Display = pvsimple.Show(clip1, renderView1)
-		ColorBar.Visibility = 1
-		pvsimple.SaveScreenshot("{}/ClipCapture.png".format(PathVL.POSTASTER), renderView1, ImageResolution=[1400, 590], FontScaling='Do not scale fonts')
-		pvsimple.Hide(clip1, renderView1)
-		print("Created image ClipCapture.png")
-
-		pvsimple.Show(thermalrmed, renderView1)
-		pvsimple.ColorBy(thermalrmedDisplay, None)
-		thermalrmedDisplay.Representation = 'Surface With Edges'
-		thermalrmedDisplay.EdgeColor = [0.0, 0.0, 0.0]
-		thermalrmedDisplay.DiffuseColor = [0.2, 0.75, 0.996078431372549]
-		pvsimple.SaveScreenshot("{}/Mesh.png".format(PathVL.POSTASTER), renderView1, ImageResolution=[1400, 590], FontScaling='Do not scale fonts')
-		pvsimple.Hide(thermalrmed, renderView1)
-		print("Created image Mesh.png")
-
-		calculator1 = pvsimple.Calculator(Input=thermalrmed)
-		calculator1.ResultArrayName = 'Resulti'
-		calculator1.Function = 'coords.iHat'
-		calculator2 = pvsimple.Calculator(Input=calculator1)
-		calculator2.ResultArrayName = 'Resultj'
-		calculator2.Function = 'coords.jHat'
-		calculator3 = pvsimple.Calculator(Input=calculator2)
-		calculator3.ResultArrayName = 'Resultk'
-		calculator3.Function = 'coords.kHat'
-		threshold1 = pvsimple.Threshold(Input=calculator3)
-		threshold1Display = pvsimple.Show(threshold1, renderView1)
-		threshold1Display.Representation = 'Surface With Edges'
-		threshold1.Scalars = ['POINTS', 'Resultj']
-		threshold1.ThresholdRange = [0, 0.0063]
-		threshold1Display.EdgeColor = [0.0, 0.0, 0.0]
-		threshold1Display.DiffuseColor = [0.2, 0.75, 0.996078431372549]
-
-		renderView1.CameraPosition = [0, -0.015, 0.00125]
-		renderView1.CameraFocalPoint = [0, 0, 0.00125]
-		renderView1.CameraViewUp = [0.0, 0.0, 1.0]
-
-		pvsimple.SaveScreenshot("{}/MeshCrossSection.png".format(PathVL.POSTASTER), renderView1, ImageResolution=[1400, 590], FontScaling='Do not scale fonts')
-		pvsimple.Hide(threshold1, renderView1)
-		print("Created image MeshCrossSection.png")
-
-		for source in pvsimple.GetSources().values():
-				pvsimple.Delete(source)
-
-		globals().update(locals())
-
-if __name__=='__main__':
-	ParaVis()

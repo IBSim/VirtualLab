e2d19253517ac96d09cf8aafe3aee912b7ba700e
Rhydian Lewis <930500@swansea.ac.uk>
2020-12-08 11:42:56 +0000

    Enabled specifying ERMES exec in VLconfig

--- Scripts/Common/VLPackages/ERMES/clsERMES.py
+++ Scripts/Common/VLPackages/ERMES/clsERMES.py
@@ -1,13 +1,13 @@
 from subprocess import Popen
 import sys
+import VLconfig
 
 class ERMES():
     def __init__(self):
-        self.Exec = 'ERMESv12.5'
+        self.Exec = getattr(VLconfig,'ERMESExec','ERMESv12.5')
 
     def Run(self,Name,**kwargs):
         cwd = kwargs.get('cwd',None)
-        # ERMES_run = Popen(Ermesstr, stdout=sys.stdout, stderr=sys.stdout, shell = 'TRUE')
         ERMES_run = Popen([self.Exec, Name], stdout=sys.stdout, stderr=sys.stderr, cwd=cwd)
         err = ERMES_run.wait()
         return err

c8fccb35a259dd165a977bbbfb3a900f517bbab5
Rhydian Lewis <930500@swansea.ac.uk>
2020-12-09 11:12:10 +0000

    Changed to dev work

--- Scripts/Common/VLTypes/Mesh.py
+++ Scripts/Common/VLTypes/Mesh.py
@@ -8,119 +8,115 @@ from importlib import import_module
 from pathos.multiprocessing import ProcessPool
 from ..VLPackages import Salome
 
-class Mesh():
-    def __init__(self,VL,**kwargs):
+def Setup(VL, **kwargs):
+    VL.MESH_DIR = "{}/Meshes".format(VL.PROJECT_DIR)
+    os.makedirs(VL.MESH_DIR, exist_ok=True)
 
-        self.Data = {}
+    VL.MeshData = {}
 
-        self.__dict__.update(VL.__dict__)
-        self.GEOM_DIR = '{}/Geom'.format(self.TMP_DIR)
+    if not kwargs.get('RunMesh', True): return
 
-        # Take what's necessary from VL class
-        self.Logger = VL.Logger
-        self.Exit = VL.Exit
-        self.WriteModule = VL.WriteModule
+    MeshDicts = VL.CreateParameters(VL.Parameters_Master, VL.Parameters_Var,'Mesh')
 
-        self.Salome = Salome.Salome(self)
+    VL.GEOM_DIR = '{}/Geom'.format(VL.TMP_DIR)
+    os.makedirs(VL.GEOM_DIR, exist_ok=True)
 
-    def Setup(self, MeshDicts,**kwargs):
-        os.makedirs(self.MESH_DIR, exist_ok=True)
-        os.makedirs(self.GEOM_DIR, exist_ok=True)
-        sys.path.insert(0, self.SIM_MESH)
+    sys.path.insert(0, VL.SIM_MESH)
 
-        for MeshName, ParaDict in MeshDicts.items():
-        	## Run checks ##
-        	# Check that mesh file exists
-        	if not os.path.exists('{}/{}.py'.format(self.SIM_MESH,ParaDict['File'])):
-        		self.Exit("Mesh file '{}' does not exist in {}".format(ParaDict['File'], self.SIM_MESH))
+    for MeshName, ParaDict in MeshDicts.items():
+    	## Run checks ##
+    	# Check that mesh file exists
+    	if not os.path.exists('{}/{}.py'.format(VL.SIM_MESH,ParaDict['File'])):
+    		VL.Exit("Mesh file '{}' does not exist in {}".format(ParaDict['File'], VL.SIM_MESH))
 
-        	MeshFile = import_module(ParaDict['File'])
-        	try :
-        		err = MeshFile.GeomError(Namespace(**ParaDict))
-        		if err: self.Exit("GeomError in '{}' - {}".format(MeshDict['Name'], err))
-        	except AttributeError:
-        		pass
-        	## Checks complete ##
+    	MeshFile = import_module(ParaDict['File'])
+    	try :
+    		err = MeshFile.GeomError(Namespace(**ParaDict))
+    		if err: VL.Exit("GeomError in '{}' - {}".format(MeshDict['Name'], err))
+    	except AttributeError:
+    		pass
+    	## Checks complete ##
 
-        	self.WriteModule("{}/{}.py".format(self.GEOM_DIR, MeshName), ParaDict)
-        	self.Data[MeshName] = Namespace(**ParaDict)
+    	VL.WriteModule("{}/{}.py".format(VL.GEOM_DIR, MeshName), ParaDict)
+    	VL.MeshData[MeshName] = Namespace(**ParaDict)
 
 
-    def PoolRun(self,MeshName,**kwargs):
+def PoolRun(VL,MeshName,**kwargs):
 
-        script = '{}/VLPackages/Salome/MeshRun.py'.format(self.COM_SCRIPTS)
-        AddPath = [self.SIM_MESH, self.GEOM_DIR]
-        Returnfile = "{}/{}_RC.txt".format(self.GEOM_DIR,MeshName) # File where salome can write an exit status to
-        ArgDict = {'Name':MeshName,
-        				'MESH_FILE':"{}/{}.med".format(self.MESH_DIR, MeshName),
-        				'RCfile':Returnfile}
-        if os.path.isfile('{}/config.py'.format(self.SIM_MESH)): ArgDict["ConfigFile"] = True
+    script = '{}/VLPackages/Salome/MeshRun.py'.format(VL.COM_SCRIPTS)
+    AddPath = [VL.SIM_MESH, VL.GEOM_DIR]
+    Returnfile = "{}/{}_RC.txt".format(VL.GEOM_DIR,MeshName) # File where salome can write an exit status to
+    ArgDict = {'Name':MeshName,
+    				'MESH_FILE':"{}/{}.med".format(VL.MESH_DIR, MeshName),
+    				'RCfile':Returnfile}
+    if os.path.isfile('{}/config.py'.format(VL.SIM_MESH)): ArgDict["ConfigFile"] = True
 
-        err = self.Salome.Run(script, AddPath=AddPath, ArgDict=ArgDict)
-        if err:
-            self.Logger("Error code {} returned in Salome run".format(err))
-            return err
+    err = VL.Salome.Run(script, AddPath=AddPath, ArgDict=ArgDict)
+    if err:
+        VL.Logger("Error code {} returned in Salome run".format(err))
+        return err
 
-        if os.path.isfile(Returnfile):
-            with open(Returnfile,'r') as f:
-                return int(f.readline())
+    if os.path.isfile(Returnfile):
+        with open(Returnfile,'r') as f:
+            return int(f.readline())
 
-    def Run(self,**kwargs):
-        MeshCheck = kwargs.get('MeshCheck', None)
-        ShowMesh = kwargs.get('ShowMesh', False)
-        NumThreads = kwargs.get('NumThreads',1)
+def devRun(VL,**kwargs):
+    kwargs.update(VL.GetArgParser()) # Update with any kwarg passed in the call
+    
+    MeshCheck = kwargs.get('MeshCheck', None)
+    ShowMesh = kwargs.get('ShowMesh', False)
+    NumThreads = kwargs.get('NumThreads',1)
 
-        # MeshCheck routine which allows you to mesh in the GUI (Used for debugging).
-        # The script will terminate after this routine
-        if MeshCheck and MeshCheck in self.Data.keys():
-            self.Logger('### Meshing {} in GUI ###\n'.format(MeshCheck), Print=True)
-            # The file MeshParaFile is passed to MeshScript to create the mesh in the GUI
-            MeshParaFile = "{}/{}.py".format(self.GEOM_DIR,MeshCheck)
-            MeshScript = "{}/{}.py".format(self.SIM_MESH, self.Data[MeshCheck].File)
+    # MeshCheck routine which allows you to mesh in the GUI (Used for debugging).
+    # The script will terminate after this routine
+    if MeshCheck and MeshCheck in VL.Data.keys():
+        VL.Logger('### Meshing {} in GUI ###\n'.format(MeshCheck), Print=True)
+        # The file MeshParaFile is passed to MeshScript to create the mesh in the GUI
+        MeshParaFile = "{}/{}.py".format(VL.GEOM_DIR,MeshCheck)
+        MeshScript = "{}/{}.py".format(VL.SIM_MESH, VL.Data[MeshCheck].File)
 
-            self.Salome.Run(MeshScript, ArgList=[MeshParaFile], GUI=True)
-            self.Exit('Terminating after checking mesh')
+        VL.Salome.Run(MeshScript, ArgList=[MeshParaFile], GUI=True)
+        VL.Exit('Terminating after checking mesh')
 
-        elif MeshCheck and MeshCheck not in self.Data.keys():
-            self.Exit("Error: '{}' specified for MeshCheck is not one of meshes to be created.\n"\
-            	         "Meshes to be created are:{}".format(MeshCheck, list(self.Data.keys())))
+    elif MeshCheck and MeshCheck not in VL.Data.keys():
+        VL.Exit("Error: '{}' specified for MeshCheck is not one of meshes to be created.\n"\
+        	         "Meshes to be created are:{}".format(MeshCheck, list(VL.Data.keys())))
 
-        self.Logger('\n### Starting Meshing ###\n',Print=True)
+    VL.Logger('\n### Starting Meshing ###\n',Print=True)
 
-        NumMeshes = len(self.Data)
-        NumThreads = min(NumThreads,NumMeshes)
+    NumMeshes = len(VL.MeshData)
+    NumThreads = min(NumThreads,NumMeshes)
 
-        Arg1 = list(self.Data.keys())
+    Arg0 = [VL]*len(VL.MeshData)
+    Arg1 = list(VL.MeshData.keys())
 
-        if True:
-            pool = ProcessPool(nodes=NumThreads)
-            Res = pool.map(self.PoolRun, Arg1)
+    if 1:
+        pool = ProcessPool(nodes=NumThreads)
+        Res = pool.map(PoolRun, Arg0, Arg1)
+    else :
+        from pyina.launchers import MpiPool
+        pool = MpiPool(nodes=NumThreads)
+        Res = pool.map(PoolRun, Arg0, Arg1)
+
+    MeshError = []
+    for Name, RC in zip(Arg1,Res):
+        if RC:
+            MeshError.append(Name)
+            VL.Logger("'{}' finished with errors".format(Name),Print=True)
         else :
-            Arg0 = [self]*len(self.Data)
-            from pyina.ez_map import ez_map
-            from pyina.launchers import  mpirun_launcher, srun_launcher
-            Res = ez_map(extRun, Arg0, Arg1, nodes=NumThreads,launcher=mpirun_launcher)
+            VL.Logger("'{}' completed successfully".format(Name), Print=True)
 
-        MeshErrors = []
-        for Name, RC in zip(Arg1,Res):
-            if RC:
-                MeshErrors.append(Name)
-                self.Logger("'{}' finished with errors".format(Name),Print=True)
-            else :
-                self.Logger("'{}' completed successfully".format(Name), Print=True)
+    if MeshError:
+        VL.Exit("The following Meshes finished with errors:\n{}".format(MeshError),KeepDirs=['Geom'])
 
+    VL.Logger('\n### Meshing Completed ###',Print=True)
+    if ShowMesh:
+        VL.Logger("Opening mesh files in Salome",Print=True)
+        ArgDict = {name:"{}/{}.med".format(VL.MESH_DIR, name) for name in VL.MeshData.keys()}
+        Script = '{}/VLPackages/Salome/ShowMesh.py'.format(VL.COM_SCRIPTS)
+        VL.Salome.Run(Script, ArgDict=ArgDict, GUI=True)
+        VL.Exit("Terminating after mesh viewing")
 
-        self.Logger('\n### Meshing Completed ###',Print=True)
-        if ShowMesh:
-            self.Logger("Opening mesh files in Salome",Print=True)
-            ArgDict = {name:"{}/{}.med".format(self.MESH_DIR, name) for name in self.Data.keys()}
-            Script = '{}/VLPackages/Salome/ShowMesh.py'.format(self.COM_SCRIPTS)
-            self.Salome.Run(Script, ArgDict=ArgDict, GUI=True)
-            self.Exit("Terminating after mesh viewing")
-
-    def Cleanup():
-        # TODO specify what we want to do at the end
-        pass
-
-def extRun(Meta,arg1):
-    Mesh.PoolRun(Meta,arg1)
+def Cleanup():
+    # TODO specify what we want to do at the end
+    pass
--- Scripts/Common/VLTypes/Sim.py
+++ Scripts/Common/VLTypes/Sim.py
@@ -6,177 +6,210 @@ from types import SimpleNamespace as Namespace
 from importlib import import_module
 from contextlib import redirect_stderr, redirect_stdout
 from pathos.multiprocessing import ProcessPool
+import copy
+# from Scripts.Common.VLPackages import CodeAster
 from ..VLPackages import CodeAster
 
-class Sim():
-    def __init__(self,VL):
+def CheckFile(Directory,fname,ext):
+    if not fname:
+        return True
+    else:
+        return os.path.isfile('{}/{}.{}'.format(Directory,fname,ext))
 
-        self.Data = {}
+def Setup(VL,**kwargs):
 
-        self.__dict__.update(VL.__dict__)
+    os.makedirs(VL.STUDY_DIR, exist_ok=True)
+    VL.SimData = {}
 
-        # Take what's necessary from VL class
-        self.Logger = VL.Logger
-        self.Exit = VL.Exit
-        self.WriteModule = VL.WriteModule
+    if not kwargs.get('RunSim', True): return
 
-        self.CodeAster = CodeAster.CodeAster(VL)
+    MetaInfo = {key:val for key,val in VL.__dict__.items() if type(val)==str}
 
-    def Setup(self,SimDicts,**kwargs):
-        os.makedirs(self.STUDY_DIR, exist_ok=True)
-        MetaInfo = {key:val for key,val in self.__dict__.items() if type(val)==str}
-        self.MeshNames = []
-        for SimName, ParaDict in SimDicts.items():
-            '''
-    		# Run checks
-    		# Check files exist
-    		if not self.__CheckFile__(self.SIM_PREASTER,ParaDict.get('PreAsterFile'),'py'):
-    			self.Exit("PreAsterFile '{}.py' not in directory {}".format(ParaDict['PreAsterFile'],self.SIM_PREASTER))
-    		if not self.__CheckFile__(self.SIM_ASTER,ParaDict.get('AsterFile'),'comm'):
-    			self.Exit("AsterFile '{}.comm' not in directory {}".format(ParaDict['AsterFile'],self.SIM_ASTER,))
-    		if not self.__CheckFile__(self.SIM_POSTASTER, ParaDict.get('PostAsterFile'), 'py'):
-    			self.Exit("PostAsterFile '{}.py' not in directory {}".format(ParaDict['PostAsterFile'],self.SIM_POSTASTER))
-    		# Check mesh will be available
-    		if not (ParaDict['Mesh'] in self.MeshData or self.__CheckFile__(self.MESH_DIR, ParaDict['Mesh'], 'med')):
-    			self.Exit("Mesh '{}' isn't being created and is not in the mesh directory '{}'".format(ParaDict['Mesh'], self.MESH_DIR))
-    		# Check materials used
-    		Materials = ParaDict.get('Materials',[])
-    		if type(Materials)==str: Materials = [Materials]
-    		elif type(Materials)==dict: Materials = Materials.values()
-    		MatErr = [mat for mat in set(Materials) if not os.path.isdir('{}/{}'.format(self.MATERIAL_DIR, mat))]
-    		if MatErr:
-    				self.Exit("Material(s) {} specified for {} not available.\n"\
-    				"Please see the materials directory {} for options.".format(MatErr,SimName,self.MATERIAL_DIR))
-    		# Checks complete
-            '''
-            # Create dict of simulation specific information to be nested in SimData
-            StudyDict = {}
-            StudyDict['TMP_CALC_DIR'] = TMP_CALC_DIR = "{}/{}".format(self.TMP_DIR, SimName)
-            StudyDict['CALC_DIR'] = CALC_DIR = "{}/{}".format(self.STUDY_DIR, SimName)
-            StudyDict['PREASTER'] = "{}/PreAster".format(CALC_DIR)
-            StudyDict['ASTER'] = "{}/Aster".format(CALC_DIR)
-            StudyDict['POSTASTER'] = "{}/PostAster".format(CALC_DIR)
-            StudyDict['MeshFile'] = "{}/{}.med".format(self.MESH_DIR, ParaDict['Mesh'])
+    SimDicts = VL.CreateParameters(VL.Parameters_Master, VL.Parameters_Var,'Sim')
+    for SimName, ParaDict in SimDicts.items():
+        # Run checks
+        # Check files exist
+        if not CheckFile(VL.SIM_PREASTER,ParaDict.get('PreAsterFile'),'py'):
+        	VL.Exit("PreAsterFile '{}.py' not in directory {}".format(ParaDict['PreAsterFile'],VL.SIM_PREASTER))
+        if not CheckFile(VL.SIM_ASTER,ParaDict.get('AsterFile'),'comm'):
+        	VL.Exit("AsterFile '{}.comm' not in directory {}".format(ParaDict['AsterFile'],VL.SIM_ASTER,))
+        if not CheckFile(VL.SIM_POSTASTER, ParaDict.get('PostAsterFile'), 'py'):
+        	VL.Exit("PostAsterFile '{}.py' not in directory {}".format(ParaDict['PostAsterFile'],VL.SIM_POSTASTER))
+        # Check mesh will be available
+        if not (ParaDict['Mesh'] in VL.MeshData or CheckFile(VL.MESH_DIR, ParaDict['Mesh'], 'med')):
+        	VL.Exit("Mesh '{}' isn't being created and is not in the mesh directory '{}'".format(ParaDict['Mesh'], VL.MESH_DIR))
+        # Check materials used
+        Materials = ParaDict.get('Materials',[])
+        if type(Materials)==str: Materials = [Materials]
+        elif type(Materials)==dict: Materials = Materials.values()
+        MatErr = [mat for mat in set(Materials) if not os.path.isdir('{}/{}'.format(VL.MATERIAL_DIR, mat))]
+        if MatErr:
+        		VL.Exit("Material(s) {} specified for {} not available.\n"\
+        		"Please see the materials directory {} for options.".format(MatErr,SimName,VL.MATERIAL_DIR))
+        # Checks complete
 
-            # Create tmp directory & add __init__ file so that it can be treated as a package
-            if not os.path.isdir(TMP_CALC_DIR): os.makedirs(TMP_CALC_DIR)
-            with open("{}/__init__.py".format(TMP_CALC_DIR),'w') as f: pass
-            # Combine Meta information with that from Study dict and write to file for salome/CodeAster to import
-            self.WriteModule("{}/PathVL.py".format(TMP_CALC_DIR), {**MetaInfo, **StudyDict})
-            # Write Sim Parameters to file for Salome/CodeAster to import
-            self.WriteModule("{}/Parameters.py".format(TMP_CALC_DIR), ParaDict)
+        # Create dict of simulation specific information to be nested in SimData
+        StudyDict = {}
+        StudyDict['TMP_CALC_DIR'] = TMP_CALC_DIR = "{}/{}".format(VL.TMP_DIR, SimName)
+        StudyDict['CALC_DIR'] = CALC_DIR = "{}/{}".format(VL.STUDY_DIR, SimName)
+        StudyDict['PREASTER'] = "{}/PreAster".format(CALC_DIR)
+        StudyDict['ASTER'] = "{}/Aster".format(CALC_DIR)
+        StudyDict['POSTASTER'] = "{}/PostAster".format(CALC_DIR)
+        StudyDict['MeshFile'] = "{}/{}.med".format(VL.MESH_DIR, ParaDict['Mesh'])
 
-            # Attach Parameters to StudyDict for ease of access
-            StudyDict['Parameters'] = Namespace(**ParaDict)
-            # Add StudyDict to SimData dictionary
-            self.Data[SimName] = StudyDict.copy()
+        # Create tmp directory & add __init__ file so that it can be treated as a package
+        if not os.path.isdir(TMP_CALC_DIR): os.makedirs(TMP_CALC_DIR)
+        with open("{}/__init__.py".format(TMP_CALC_DIR),'w') as f: pass
+        # Combine Meta information with that from Study dict and write to file for salome/CodeAster to import
+        VL.WriteModule("{}/PathVL.py".format(TMP_CALC_DIR), {**MetaInfo, **StudyDict})
+        # Write Sim Parameters to file for Salome/CodeAster to import
+        VL.WriteModule("{}/Parameters.py".format(TMP_CALC_DIR), ParaDict)
 
-            self.MeshNames.append(ParaDict['Mesh'])
+        # Attach Parameters to StudyDict for ease of access
+        StudyDict['Parameters'] = Namespace(**ParaDict)
+        # Add StudyDict to SimData dictionary
+        VL.SimData[SimName] = StudyDict.copy()
 
-    def PoolRun(self, StudyDict, kwargs):
-        RunPreAster = kwargs.get('RunPreAster',True)
-        RunAster = kwargs.get('RunAster', True)
-        RunPostAster = kwargs.get('RunPostAster', True)
+def PoolRun(VL, StudyDict, kwargs):
+    RunPreAster = kwargs.get('RunPreAster',True)
+    RunAster = kwargs.get('RunAster', True)
+    RunPostAster = kwargs.get('RunPostAster', True)
 
-        mpi_nbcpu = kwargs.get('mpi_nbcpu',1)
-        mpi_nbnoeud = kwargs.get('mpi_nbnoeud',1)
-        ncpus = kwargs.get('ncpus',1)
-        memory = kwargs.get('memory',2)
+    Parameters = StudyDict["Parameters"]
 
-        Parameters = StudyDict["Parameters"]
-        SimLogFile = "{}/Output.log"
+    if VL.mode == 'Headless':OutFile = "{}/Output.log".format(StudyDict['TMP_CALC_DIR'])
+    elif VL.mode == 'Continuous':OutFile = "{}/Output.log".format(StudyDict['TMP_CALC_DIR'])
+    else : OutFile=''
 
-        if self.mode == 'Headless': OutFile = "{}/Output.log"
-        elif self.mode == 'Continuous': OutFile = "{}/Output.log"
-        else : OutFile=''
+    # Copy original dictionary so that we can tell if anything gets added to it
+    OrigDict = copy.deepcopy(StudyDict)
+    Error=None
 
-        if RunPreAster and hasattr(Parameters,'PreAsterFile'):
-            sys.path.insert(0, self.SIM_PREASTER)
-            PreAster = import_module(Parameters.PreAsterFile)
-            PreAsterSgl = getattr(PreAster, 'Single',None)
+    # Create a blank file
+    if OutFile:
+        with open(OutFile,'w') as f: pass
 
-            self.Logger("Running PreAster for '{}'\n".format(Parameters.Name),Print=True)
-            os.makedirs(StudyDict['PREASTER'],exist_ok=True)
+    if RunPreAster and hasattr(Parameters,'PreAsterFile'):
+        sys.path.insert(0, VL.SIM_PREASTER)
+        PreAster = import_module(Parameters.PreAsterFile)
+        PreAsterSgl = getattr(PreAster, 'Single',None)
 
-            if not OutFile:
-                PreAsterSgl(self,StudyDict)
-            else :
-                with open(OutFile.format(StudyDict['PREASTER']),'w') as f:
-                    with redirect_stdout(f), redirect_stderr(f):
-                        PreAsterSgl(self,StudyDict)
+        VL.Logger("Running PreAster for '{}'\n".format(Parameters.Name),Print=True)
+        os.makedirs(StudyDict['PREASTER'],exist_ok=True)
 
-        if RunAster and hasattr(Parameters,'AsterFile'):
-            self.Logger("Running Aster for '{}'\n".format(Parameters.Name),Print=True)
-
-            os.makedirs(StudyDict['ASTER'],exist_ok=True)
-            # Create export file for CodeAster
-            ExportFile = "{}/Export".format(StudyDict['ASTER'])
-            CommFile = '{}/{}.comm'.format(self.SIM_ASTER, Parameters.AsterFile)
-            MessFile = '{}/AsterLog'.format(StudyDict['ASTER'])
-            self.CodeAster.ExportWriter(ExportFile, CommFile,
-            							StudyDict["MeshFile"],
-            							StudyDict['ASTER'], MessFile)
-
-            if self.mode == 'Headless': AsterOut='/dev/null'
-            elif self.mode == 'Continuous': AsterOut = OutFile.format(StudyDict['ASTER'])
-            else : AsterOut=''
-
-            SubProc = self.CodeAster.Run(ExportFile, OutFile=AsterOut, AddPath=[self.TMP_DIR,StudyDict['TMP_CALC_DIR']])
-            SubProc.wait()
-            # from subprocess import Popen
-            # SubProc = Popen(['echo','Hello World'])
-            # SubProc.wait()
-
-        if RunPostAster and hasattr(Parameters,'PostAsterFile'):
-            sys.path.insert(0, self.SIM_POSTASTER)
-            PostAster = import_module(Parameters.PostAsterFile)
-            PostAsterSgl = getattr(PostAster, 'Single', None)
-
-            self.Logger("Running PostAster for '{}'\n".format(Parameters.Name),Print=True)
-            os.makedirs(StudyDict['POSTASTER'],exist_ok=True)
-
-            PostAsterSgl(self,StudyDict)
-            if not OutFile:
-                PostAsterSgl(self,StudyDict)
-            else :
-                with open(OutFile.format(StudyDict['POSTASTER']),'w') as f:
-                    with redirect_stdout(f), redirect_stderr(f):
-                        PostAsterSgl(self,StudyDict)
-
-
-
-    def Run(self,**kwargs):
-        ShowRes = kwargs.get('ShowRes', False)
-        NumThreads = kwargs.get('NumThreads',1)
-
-        # Run high throughput part in parallel
-        NumSim = len(self.Data)
-        NumThreads = min(NumThreads,NumSim)
-
-        Arg1 = list(self.Data.values())
-        Arg2 = [kwargs]*NumSim
-        if 1:
-            pool = ProcessPool(nodes=NumThreads)
-            Res = pool.map(self.PoolRun, Arg1, Arg2)
+        if not OutFile:
+            err = PreAsterSgl(VL,StudyDict)
         else :
-            Arg0 = [self]*NumSim
-            from pyina.launchers import MpiPool
-            pool = MpiPool(nodes=NumThreads,source=True)
-            res = pool.map(extRun, Arg0, Arg1, Arg2)
-        # 
-        #
-        # PostAster = import_module(Parameters_Master.Sim.PostAsterFile)
-        # if hasattr(PostAster, 'Combined'):
-        #     self.Logger('Combined function started', Print=True)
-        #     if self.mode in ('Interactive','Terminal'):
-        #         err = PostAster.Combined(self)
-        #     else :
-        #         with open(self.LogFile, 'a') as f:
-        #             with redirect_stdout(f), redirect_stderr(f):
-        #                 err = PostAster.Combined(self)
+            with open(OutFile,'a') as f:
+                with redirect_stdout(f), redirect_stderr(f):
+                    err = PreAsterSgl(VL,StudyDict)
 
+        if err:
+            Error = ['PreAster',err]
+            RunAster=RunPostAster=False
 
+    if RunAster and hasattr(Parameters,'AsterFile'):
+        VL.Logger("Running Aster for '{}'\n".format(Parameters.Name),Print=True)
 
-# Can't use class method with pyina so create this function which calls the method
-def extRun(Meta,arg1,arg2):
-    Meta.PoolRun(arg1,arg2)
+        os.makedirs(StudyDict['ASTER'],exist_ok=True)
+        # Create export file for CodeAster
+        ExportFile = "{}/Export".format(StudyDict['ASTER'])
+        CommFile = '{}/{}.comm'.format(VL.SIM_ASTER, Parameters.AsterFile)
+        MessFile = '{}/AsterLog'.format(StudyDict['ASTER'])
+        VL.CodeAster.ExportWriter(ExportFile, CommFile,
+        							StudyDict["MeshFile"],
+        							StudyDict['ASTER'],
+                                    MessFile, **kwargs)
+
+        if VL.mode == 'Headless': AsterOut='/dev/null'
+        elif VL.mode == 'Continuous': AsterOut = OutFile.format(StudyDict['ASTER'])
+        else : AsterOut=''
+
+        SubProc = VL.CodeAster.Run(ExportFile, Name=Parameters.Name,
+                                     AddPath=[VL.TMP_DIR,StudyDict['TMP_CALC_DIR']],
+                                     OutFile=AsterOut)
+        err = SubProc.wait()
+        if err:
+            Error = ['Aster',"Aster SubProc returned code {}".format(err)]
+            RunPostAster=False
+
+        # from subprocess import Popen
+        # SubProc = Popen(['echo','Hello World'])
+        # SubProc.wait()
+
+    if RunPostAster and hasattr(Parameters,'PostAsterFile'):
+        sys.path.insert(0, VL.SIM_POSTASTER)
+        PostAster = import_module(Parameters.PostAsterFile)
+        PostAsterSgl = getattr(PostAster, 'Single', None)
+
+        VL.Logger("Running PostAster for '{}'\n".format(Parameters.Name),Print=True)
+        os.makedirs(StudyDict['POSTASTER'],exist_ok=True)
+
+        if not OutFile:
+            err = PostAsterSgl(VL,StudyDict)
+        else :
+            with open(OutFile,'a') as f:
+                with redirect_stdout(f), redirect_stderr(f):
+                    err = PostAsterSgl(VL,StudyDict)
+        if err:
+            Error = ['PostAster',err]
+
+    # Data structure which can be easily returned holdign additional information
+    Returner = Namespace(Error=Error)
+    # Add StudyDict to returned if it's changed
+    if not OrigDict == StudyDict:
+        Returner.StudyDict = StudyDict
+
+    return Returner
+
+def devRun(VL,**kwargs):
+    kwargs.update(VL.GetArgParser()) # Update with any kwarg passed in the call
+
+    ShowRes = kwargs.get('ShowRes', False)
+    NumThreads = kwargs.get('NumThreads',1)
+
+    # Run high throughput part in parallel
+    NumSim = len(VL.SimData)
+    NumThreads = min(NumThreads,NumSim)
+
+    Arg0 = [VL]*NumSim
+    Arg1 = list(VL.SimData.values())
+    Arg2 = [kwargs]*NumSim
+    if 1:
+        pool = ProcessPool(nodes=NumThreads)
+        Res = pool.map(PoolRun, Arg0, Arg1, Arg2)
+    else :
+        from pyina.launchers import MpiPool
+        pool = MpiPool(nodes=NumThreads)
+        Res = pool.map(PoolRun, Arg0, Arg1, Arg2)
+
+    SimError = []
+    for Name, Returner in zip(VL.SimData.keys(),Res):
+        if Returner.Error:
+            SimError.append(Name)
+            VL.Logger("'{}' finished with errors".format(Name),Print=True)
+        else :
+            VL.Logger("'{}' completed successfully".format(Name), Print=True)
+        if hasattr(Returner,'StudyDict'):
+            VL.SimData[Name].update(Returner.StudyDict)
+
+    if SimError:
+        VL.Exit("The following Simulation routine(s) finished with errors:\n{}".format(SimError))
+
+    PostAster = getattr(VL.Parameters_Master.Sim, 'PostAsterFile', None)
+    if PostAster:
+        sys.path.insert(0, VL.SIM_POSTASTER)
+        PostAster = import_module(PostAster)
+        if hasattr(PostAster, 'Combined'):
+            VL.Logger('Combined function started', Print=True)
+            if VL.mode in ('Interactive','Terminal'):
+                err = PostAster.Combined(VL)
+            else :
+                with open(VL.LogFile, 'a') as f:
+                    with redirect_stdout(f), redirect_stderr(f):
+                        err = PostAster.Combined(VL)
+
+            if err == None:
+                VL.Logger('Combined function completed successfully', Print=True)
+            else :
+                VL.Exit("Combined function returned error '{}'".format(err))

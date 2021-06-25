a25573cd65e24230d86a7ceea8eb12dd3fed51fa
Rhydian Lewis <930500@swansea.ac.uk>
2021-06-25 09:59:54 +0100

    Added stl and xao formats to ExportGeom

--- Scripts/Common/VLPackages/Salome/MeshRun.py
+++ Scripts/Common/VLPackages/Salome/MeshRun.py
@@ -3,6 +3,7 @@ import os
 sys.dont_write_bytecode=True
 import SalomeFunc
 import salome
+from salome.geom import geomBuilder
 salome.salome_init()
 
 # This function gives the ArgDict dictionary we passed to SalomeRun
@@ -21,13 +22,22 @@ elif type(MeshRn)==salome.smesh.smeshBuilder.Mesh:
     isDone = MeshRn.Compute()
     SalomeFunc.MeshExport(MeshRn, MeshDict['MESH_FILE'])
 
-    if getattr(Parameters,'ExportGeom',False):
-        from salome.geom import geomBuilder
-        import GEOM
+    # If Mesh has the attribute 'Export Geom' then the geometry used to mesh will
+    # be exported alognside the mesh. Available formats are: stp,stl and xao.
+    # NOTE: xao exports all groups associated with the geomtry as well
+    if hasattr(Parameters,'ExportGeom'):
         geompy = geomBuilder.New()
         SampleGeom = MeshRn.GetShape()
-        StepFile = "{}.stp".format(os.path.splitext(MeshDict['MESH_FILE'])[0])
-        geompy.ExportSTEP(SampleGeom, StepFile, GEOM.LU_METER )
+        fname = os.path.splitext(MeshDict['MESH_FILE'])[0] # Same name as mesh 
+
+        if Parameters.ExportGeom.lower() in ('step','stp'):
+            import GEOM
+            geompy.ExportSTEP(SampleGeom, "{}.stp".format(fname), GEOM.LU_METER )
+        elif Parameters.ExportGeom.lower() == 'stl':
+            geompy.ExportSTL(SampleGeom, "{}.stl".format(fname), True, 0.001, True)
+        elif Parameters.ExportGeom.lower() == 'xao':
+            groups = geompy.GetExistingSubObjects(SampleGeom,True)
+            geompy.ExportXAO(SampleGeom, groups, [], "", "{}.xao".format(fname), "")
 
 # salome.myStudy.Clear()
 # salome.salome_close()

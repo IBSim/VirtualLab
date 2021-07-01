dfa41154c9e2d72894407eca417e54501422b7f2
Rhydian Lewis <930500@swansea.ac.uk>
2021-07-01 14:20:30 +0100

    Added Radiation and thermal contact conductance for HIVE simulation (steady state)

--- RunFiles/.devRun.py
+++ RunFiles/.devRun.py
@@ -8,25 +8,24 @@ from Scripts.Common.VirtualLab import VLSetup
 
 Simulation = 'HIVE'
 Project = '.dev'
-StudyName = 'Testing'
+StudyName = 'Test'
 Parameters_Master='Master'
 # Parameters_Var='Parametric_1'
-# Parameters_Var='Random'
 Parameters_Var=None
 Mode = "T"
 
 VirtualLab = VLSetup(Simulation,Project,StudyName,Parameters_Master,Parameters_Var,Mode)
 
 # Create directories and Parameter files for simulation
-VirtualLab.Control(RunMesh=1, RunSim=1, RunML=0)
+VirtualLab.Control(RunMesh=1, RunSim=1)
 
 # Creates meshes
-VirtualLab.devMesh(ShowMesh=0, MeshCheck=None, NumThreads=5)
+VirtualLab.devMesh(ShowMesh=0, MeshCheck=None, NumThreads=2,launcher='Process')
 
 # Run Pre-Sim calculations, CodeAster and Post-Sim calculations/imaging
-VirtualLab.devSim(RunPreAster=1, RunAster=0, RunPostAster=0, NumThreads=6, ShowRes=1, mpi_nbcpu=1, mpi_nbnoeud=1, ncpus=2, memory=10, launcher='Process',onall=False)
+VirtualLab.devSim(RunPreAster=1, RunAster=1, RunPostAster=0, NumThreads=6, ShowRes=0, mpi_nbcpu=1, mpi_nbnoeud=1, ncpus=2, memory=10, launcher='Process',onall=False)
 
-VirtualLab.devML()
+VirtualLab.devDA()
 
 # Remove tmp folders
 VirtualLab.Cleanup()

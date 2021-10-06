76e75000b7ad06519cf2265dcf4e849ee7a2b278
Rhydian Lewis <930500@swansea.ac.uk>
2021-10-06 11:21:10 +0100

    Update to ML input file

--- Input/HIVE/Tutorials/GPR_Master.py
+++ Input/HIVE/Tutorials/GPR_Master.py
@@ -5,30 +5,33 @@ DA = Namespace()
 #### Machine Learning ####
 ##########################
 
-DA.Name = 'ML/GPR'
+DA.Name = 'ML/GPR_Halton'
 
 DA.File = 'CoilConfig_GPR'
 
-DA.Device = 'cpu'
-DA.NbTorchThread = 2
-
 #========================
 #Training
-DA.Train = 1 # flag whether to train or not
+# flag whether to train or not
+DA.Train = 1
+# File where data comes from
 DA.DataFile = 'Data.hdf5'
+# Training data used
+DA.TrainData = "Halton/PU_3"
+# Nb training points used
+DA.TrainNb = 100
+# Test data used
+DA.TestData = "TestData/PU_3"
+# train/test split - decides how much test data to use
+DA.DataSplit = 0.8
+#Max. number of epoch iterations.
+DA.Iterations = 10000
+# Parameters for GPR model
 DA.Kernel = 'RBF'
 DA.Noise = False
 DA.lr = 0.01
-DA.Iterations = 10000
-DA.TrainData = "Halton/PU_3" #training data
-DA.TrainNb = 100 # Nb training points to consider
-
-DA.TestData = "TestData/PU_3"
-DA.DataSplit = 0.8 # train/test split - decides how much test data to use
 
 #=========================
 
-DA.Input = None
-
 # Optimisation 1: Find the max power
-DA.MaxPowerOpt = {'NbInit':20, 'Verify':1, 'NewSim':1}
+# Verify - runs a simulation to compare againt model prediction
+DA.MaxPowerOpt = {'NbInit':20, 'Verify':True}

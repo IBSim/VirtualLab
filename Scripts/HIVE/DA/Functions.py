5ce97bf9dd5662cd0f83ca2c9d391fc672b8edd6
Rhydian Lewis <930500@swansea.ac.uk>
2022-01-26 17:35:22 +0000

    Created multistart implementation of slsqp minimiser & incorporated in to FuncOpt routine

--- Scripts/HIVE/DA/Functions.py
+++ Scripts/HIVE/DA/Functions.py
@@ -8,6 +8,7 @@ from Scripts.Common.VLFunctions import MeshInfo
 '''
 Add in Uniformity 1 which looks at stdev
 '''
+
 def Uniformity2(JHNode, MeshFile):
     Meshcls = MeshInfo(MeshFile)
     CoilFace = Meshcls.GroupInfo('CoilFace')
@@ -77,43 +78,6 @@ def DataRescale(data,const,scale):
     scale: N-darray or scalar
     '''
     return data*scale + const
-
-def _MinMax(X, fn, sign, *args):
-    val,grad = fn(X,*args)
-    return sign*val,sign*grad
-
-def FuncOpt(fnc, NbInit, bounds, find='max',order='decreasing', tol=0.01, **kwargs):
-    if find.lower()=='max':sign=-1
-    elif find.lower()=='min':sign=1
-    _Optima, fnVal, fnGrad, Coord = [],[],[],[]
-
-    kwargs['args'] = (fnc,sign,*kwargs.get('args',[]))
-    for X0 in np.random.uniform(0,1,size=(NbInit,4)):
-        Opt = minimize(_MinMax, X0, jac=True, method='SLSQP', bounds=bounds,**kwargs)
-        _Optima.append(Opt)
-        if Opt.success:
-            fnVal.append(Opt.fun)
-            fnGrad.append(Opt.jac)
-            Coord.append(Opt.x)
-
-    fnVal,fnGrad,Coord = sign*np.array(fnVal),sign*np.array(fnGrad),np.array(Coord)
-
-    #Sort Optimas in increasing/decreasing order
-    ord = -1 if order.lower()=='decreasing' else 1
-    sortIx = np.argsort(fnVal,None)[::ord]
-    fnVal,fnGrad,Coord = fnVal[sortIx],fnGrad[sortIx],Coord[sortIx]
-
-    if tol:
-        tolCd,Ix = Coord[:1],[0]
-
-        for i, cd in enumerate(Coord[1:]):
-            D = np.linalg.norm(tolCd - cd, axis=1)
-            if (D>tol).all():
-                Ix.append(i+1)
-                tolCd = np.vstack((tolCd,cd))
-        fnVal,fnGrad,Coord = fnVal[Ix],fnGrad[Ix],Coord[Ix]
-
-    return Coord, fnVal, fnGrad
 
 def MSE(Predicted,Target):
     sqdiff = (Predicted - Target)**2

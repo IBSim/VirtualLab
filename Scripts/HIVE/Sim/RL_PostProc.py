7a963f5d245fbe2d63f14fd248951d9103c0534d
Rhydian Lewis <930500@swansea.ac.uk>
2022-02-04 14:54:10 +0000

    Updated MaxTemp script for adaptive sampling

--- Scripts/HIVE/Sim/RL_PostProc.py
+++ Scripts/HIVE/Sim/RL_PostProc.py
@@ -3,10 +3,14 @@ from AsterPostProc import TC_Temperature, MaxTemperature
 def Single(VL,SimDict):
     Parameters = SimDict["Parameters"]
     ResFile = '{}/Thermal.rmed'.format(SimDict['ASTER'])
-    TC_Temp = TC_Temperature(ResFile,Parameters.ThermoCouple)[0]
 
     MaxTemp =  MaxTemperature(ResFile)[0]
-
     SimDict['Data']['MaxTemp'] = MaxTemp
-    SimDict['Data']['TC_Temp'] = TC_Temp
+
+    if hasattr(Parameters,'ThermoCouple'):
+        TC_Temp = TC_Temperature(ResFile,Parameters.ThermoCouple)[0]
+        SimDict['Data']['TC_Temp'] = TC_Temp
+
+
+
     # SimDict['Data']['Test'] = 1

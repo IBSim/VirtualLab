aa40891e564a63d1e0d19bde4cfab36fda08ad5f
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2022-03-23 15:13:31 +0000

    new runfile to generate a fresh AMAZE mesh for tutorial 5

---
+++ RunFiles/Run_setup_Tutorial5.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python3
+import sys
+sys.dont_write_bytecode=True
+from Scripts.Common.VirtualLab import VLSetup
+
+#===============================================================================
+# Setup
+
+Simulation='HIVE'
+Project='Tutorials'
+Parameters_Master='TrainingParameters'
+Parameters_Var=None
+
+#===============================================================================
+# Environment
+
+VirtualLab=VLSetup(
+           Simulation,
+           Project)
+
+VirtualLab.Settings(
+           Mode='Headless',
+           Launcher='Process',
+           NbThreads=1)
+
+VirtualLab.Parameters(
+           Parameters_Master,
+           Parameters_Var,
+           RunMesh=True,
+           RunSim=True,
+           RunDA=True)
+
+VirtualLab.Mesh(
+           ShowMesh=False,
+           MeshCheck=None)
+
+VirtualLab.Sim(
+           RunPreAster=True,
+           RunAster=True,
+           RunPostAster=True,
+           ShowRes=False)
+
+VirtualLab.DA()
+
+VirtualLab.Cleanup()

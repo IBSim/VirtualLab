752e050eb9b8a50e734fd045e178ac33eb0b552d
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2022-05-20 22:19:11 +0000

    Update Install_Cad2Vox.sh to change cuda install to use the nvidia ppa verion of CUDA 11.6

--- Scripts/Install/Install_Cad2Vox.sh
+++ Scripts/Install/Install_Cad2Vox.sh
@@ -61,17 +61,12 @@ fi
 
 if ${CAD2VOX_WITH_CUDA}; then
     echo "Installing CUDA"
-    if ${USE_CONDA}; then
-	conda install -y cudatoolkit
-    else
     wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-ubuntu1804.pin
     sudo mv cuda-ubuntu1804.pin /etc/apt/preferences.d/cuda-repository-pin-600
-    wget https://developer.download.nvidia.com/compute/cuda/11.6.0/local_installers/cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
-    sudo dpkg -i cuda-repo-ubuntu1804-11-6-local_11.6.0-510.39.01-1_amd64.deb
-    sudo apt-key add /var/cuda-repo-ubuntu1804-11-6-local/7fa2af80.pub
+    sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
+    sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
     sudo apt-get update
     sudo apt-get -y install cuda
-    fi
 else
     echo "Skiping CUDA install"
 fi

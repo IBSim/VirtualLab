e117ead93b59f0f9f8fa2c2f0e4e69b78883e085
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2022-05-21 17:44:33 +0000

    Update Install_Cad2Vox.sh to fix CUDA version used to 11.4 this is due to an as yet un-explained bug with the latest Version 11.7

--- Scripts/Install/Install_Cad2Vox.sh
+++ Scripts/Install/Install_Cad2Vox.sh
@@ -66,7 +66,7 @@ if ${CAD2VOX_WITH_CUDA}; then
     sudo apt-key adv --fetch-keys https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/3bf863cc.pub
     sudo add-apt-repository "deb https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
     sudo apt-get update
-    sudo apt-get -y install cuda
+    sudo apt-get -y install cuda=11.4.0-1
 else
     echo "Skiping CUDA install"
 fi

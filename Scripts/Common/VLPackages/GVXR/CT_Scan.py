Fixed tet conversion to properly track mat_ids. Also added in possition units which somehow got missed.

Scripts/Common/VLPackages/GVXR/CT_Scan.py
@@ -9,7 +9,7 @@ from Scripts.Common.VLPackages.GVXR.GVXR_utils import *
 import numexpr as ne
 
 def CT_scan(mesh_file,output_file,Beam,Detector,Model,Material_file=None,Headless=False,
-num_projections = 180,angular_step=1,im_format=None,use_tetra=False):
+num_projections = 180,angular_step=1,im_format='tiff',use_tetra=False):
     ''' Main run function for GVXR'''
     # Print the libraries' version
     print (gvxr.getVersionOfSimpleGVXR())
@@ -53,18 +53,14 @@ num_projections = 180,angular_step=1,im_format=None,use_tetra=False):
         all_mat_tags=mesh.cell_tags
     except AttributeError:
         all_mat_tags = {}
-# switch element type based on flag, this prevents us having to keep checking
-#  if using tets or tri.
-    if use_tetra:
-        #extract surface triangles from volume tetrahedron mesh
-        elements = tets2tri(tetra,points)
-    else:
-        elements = triangles
 
     if not all_mat_tags:
         print ("[WARN] No materials defined in input file so we assume the whole mesh is made of a single material.")
         mat_tag_dict={0:['Un-Defined']}
-        mat_ids = np.zeros(np.shape(elements),dtype = int) 
+        if use_tetra:
+            mat_ids = np.zeros(np.shape(tetra)[0],dtype = int)
+        else:
+            mat_ids = np.zeros(np.shape(triangles)[0],dtype = int) 
         tags = np.unique(mat_ids)
     else:
     # pull the dictionary containing material id's for each element
@@ -79,7 +75,14 @@ num_projections = 180,angular_step=1,im_format=None,use_tetra=False):
             all_mat_tags[0]=['Un-Defined']
         mat_tag_dict = find_the_key(all_mat_tags, np.unique(mat_ids))
             
-
+# switch element type based on flag, this prevents us having to keep checking
+#  if using tets or tri.
+    if use_tetra:
+        #extract surface triangles from volume tetrahedron mesh
+        elements, mat_ids  = tets2tri(tetra,points,mat_ids)
+    else:
+        elements = triangles
+    
     if Material_file is None:
         Material_file = 'Materials.csv'
 
@@ -93,15 +96,17 @@ num_projections = 180,angular_step=1,im_format=None,use_tetra=False):
         Material_list = Generate_Material_File(Material_file,mat_tag_dict)
 
     meshes=[]
+    
     for N in tags:
         nodes = np.where(mat_ids==N)
         nodes=nodes[0]
         #set first value outside loop
-        mat_nodes=elements[nodes[0],np.newaxis]
-        for M in nodes[1:]:
-            mat_nodes=np.vstack([mat_nodes,elements[M]])
+        #mat_nodes=elements[nodes[0],np.newaxis]
+        #for M in nodes[1:]:
+        #    mat_nodes=np.vstack([mat_nodes,elements[M]])
+        mat_nodes = np.take(elements,nodes,axis=0)
         meshes.append(mat_nodes)
-
+    
 
     #define boundray box for mesh
     min_corner = np.array([np.min(points[:,0]), np.min(points[:,1]), np.min(points[:,2])])
@@ -145,7 +150,7 @@ num_projections = 180,angular_step=1,im_format=None,use_tetra=False):
         gvxr.makeTriangularMesh(Mesh_Name,
         points.flatten(),
         mesh.flatten(),
-        "m");
+        Model.Pos_units);
         # place mesh at the orgin then traslate it according to the defined ofset
         #gvxr.moveToCentre(Mesh_Name);
         gvxr.translateNode(Mesh_Name,Model.PosX,Model.PosY,Model.PosZ,Model.Pos_units)
@@ -186,7 +191,7 @@ num_projections = 180,angular_step=1,im_format=None,use_tetra=False):
     # calculate the rotation vector in model co-ordiantes that points
     # along the global axis
     # this is needed to alow us to rotate around the global axis rather than the cad model axis.
-    global_axis_vec = world_to_model_axis(total_rotation[:,1],global_axis=[0,0,1]) # caculate vector along global Z-axis in object co-odinates
+    global_axis_vec = world_to_model_axis(total_rotation[:,0],global_axis=[0,0,1]) # caculate vector along global Z-axis in object co-odinates
     for i in range(num_projections):
         # Compute an X-ray image and add it to the list of projections
         projections.append(gvxr.computeXRayImage());
@@ -267,7 +272,6 @@ def minus_log(arr):
     """
 
     out = ne.evaluate('-log(arr)')
-    print(out)
     return out
 
 

Scripts/Common/VLPackages/GVXR/GVXR_utils.py
@@ -8,7 +8,6 @@ def FlipNormal(triangle_index_set):
     ''' Function to swap index order of triangles to flip the surface normal.'''
     triangle_index_set = np.array(triangle_index_set)
     triangle_index_set[[0,1,2]] = triangle_index_set[[0,2,1]]
-
     return triangle_index_set
 
 def correct_normal(tri_ind,extra,points):
@@ -38,26 +37,38 @@ def extract_unique_triangles(t):
     sort_tri = np.sort(t,axis=1)
     _,index,counts = np.unique(sort_tri,axis=0,return_index=True, return_counts=True)
     tri = t[index[counts==1]]
-    return tri
+    return tri, index[counts==1]
 
-def tets2tri(tetra,points):
+def tets2tri(tetra,points,mat_ids):
     import itertools
     import time
     start = time.time()
-    surface = set()
-
-    for i,tet in enumerate(tetra):
+    # each tet has been broken dwon into 4 triangles 
+    # so we must expand mat_ids by 4 times to get the
+    #  id for each tri.
+    #mat_ids = np.repeat(mat_ids,4)
+    vol_mat_ids = np.empty(len(tetra)*4,'int')
+    surface = []
+    surf_mat_ids=[]
+    j = 0
+    for i,tet in enumerate(tetra,start=0):
         Nodes = tuple(itertools.combinations(tet,3))
         for face in Nodes:
+            vol_mat_ids[j] = mat_ids[i]
             extra = list(set(tetra[i]).difference(face))
+            #breakpoint()
             face = correct_normal(face,extra,points)
-            surface.add((face[0], face[1], face[2]))
+            surface.append([face[0], face[1], face[2]])
+            j+=1
+            
     tri = np.array(list(surface),'int32')
-    tri_surf = extract_unique_triangles(tri)
+    # extract triangles on the surface of the mesh and there id's
+    tri_surf, ind = extract_unique_triangles(tri)
+    surf_mat_ids = np.take(vol_mat_ids,ind)
     stop = time.time()
     print(f"Conversion took: {stop-start} seconds")
 
-    return tri_surf
+    return tri_surf, surf_mat_ids
 
 def Generate_Material_File(material_file,mat_tags):
     """ Function to generate a new Materail file if none are defined.
@@ -113,10 +124,10 @@ def find_the_key(dictionary:dict, target_keys:str):
     """Function to pull out the keys of a dictionary as a list."""
     return {key: dictionary[key] for key in target_keys}
 
-def write_image(output_file:str,vox:np.double,im_format:str=None):
+def write_image(output_file:str,vox:np.double,im_format:str='tiff'):
     from PIL import Image, ImageOps
     import tifffile as tf
-    if (im_format):
+    if (im_format != 'tiff'):
         for I in range(0,np.shape(vox)[2]):
             im = Image.fromarray(vox[:,:,I])
             im = ImageOps.grayscale(im)
@@ -124,7 +135,6 @@ def write_image(output_file:str,vox:np.double,im_format:str=None):
             im.save(im_output)
     else:
         im_output="{}.tiff".format(output_file)
-        #tf.imwrite(im_output,vox,photometric='minisblack')
         tf.imwrite(im_output,vox)
         
 def InitSpectrum(Beam,Verbose:bool=True,Headless:bool=False):
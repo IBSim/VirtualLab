6afad078d03be70a917bfe8554f953f5a7454995
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2022-08-17 15:23:26 +0100

    changed to using local version of coide cipiued from VL_docker

--- Scripts/Install/Install_GVXR_brew.sh
+++ Scripts/Install/Install_GVXR_brew.sh
@@ -87,8 +87,8 @@ else
 fi
 #conda install scikit-image
 #grab the GVXR Source
-wget https://sourceforge.net/projects/gvirtualxray/files/1.1/gVirtualXRay-1.1.5-Source.zip/download
-mv download gVirtualXRay-1.1.5-Source.zip
+#wget https://sourceforge.net/projects/gvirtualxray/files/1.1/gVirtualXRay-1.1.5-Source.zip/download
+mv /home/ibsim/gVirtualXRay-1.1.5-Source.zip ${GVXR_DIR}
 unzip gVirtualXRay-1.1.5-Source.zip
 cd gVirtualXRay-1.1.5
 mkdir -p bin-release
@@ -106,7 +106,6 @@ cmake -DCMAKE_BUILD_TYPE:STRING=Release \
 -DBUILD_WRAPPER_RUBY:BOOL=OFF \
 -DBUILD_WRAPPER_TCL:BOOL=OFF \
 -DUSE_LIBTIFF:BOOL=OFF \
--DCMAKE_POLICY_DEFAULT_CMP0072=NEW \
 -S .. -B $PWD
 
 # Bodge to fix ubuntu specifc issues
@@ -116,19 +115,7 @@ cmake -DCMAKE_BUILD_TYPE:STRING=Release \
 # errors with swig which have yet to be addressed by Frank.
 make assimp -j12
 cmake -DCMAKE_BUILD_TYPE:STRING=Release \
--DCMAKE_INSTALL_PREFIX:STRING=$GVXR_INSTALL_DIR \
--DBUILD_TESTING:BOOL=OFF \
--DBUILD_WRAPPER_CSHARP:BOOL=OFF \
--DBUILD_WRAPPER_JAVA:BOOL=OFF \
--DBUILD_WRAPPER_OCTAVE:BOOL=OFF \
--DBUILD_WRAPPER_PERL:BOOL=OFF \
--DBUILD_WRAPPER_PYTHON3:BOOL=ON \
--DBUILD_WRAPPER_R:BOOL=OFF \
--DBUILD_WRAPPER_RUBY:BOOL=OFF \
--DBUILD_WRAPPER_TCL:BOOL=OFF \
--DUSE_LIBTIFF:BOOL=OFF \
--DCMAKE_POLICY_DEFAULT_CMP0072=NEW \
--S .. -B $PWD
+
 make glew -j12
 mkdir -p gvxr/glew-install/lib64
 cp gvxr/glew-install/lib/lib*.a gvxr/glew-install/lib64

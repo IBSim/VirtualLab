c057261482968fd5611be5b54d2ccc4affbe0356
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2022-08-24 11:09:39 +0100

    More functions to run CIL

--- Scripts/Common/VLPackages/CIL/CT_reconstruction.py
+++ Scripts/Common/VLPackages/CIL/CT_reconstruction.py
@@ -4,8 +4,6 @@ import SimpleITK as sitk
 from skimage import io
 from types import SimpleNamespace as Namespace
 import os
-import glob
-import copy
 from cil.framework import AcquisitionGeometry, AcquisitionData
 
 # CIL Processors
@@ -16,66 +14,6 @@ from cil.utilities.display import show2D, show_geometry
 
 # From CIL ASTRA plugin
 from cil.plugins.astra import FBP
-
-
-def CreateParameters(Parameters_Master,Parameters_Var,VLType):
-    '''
-    Create parameter dictionary of attribute VLType using Parameters_Master and Var.
-    '''
-    # ======================================================================
-    # Get VLType from Parameters_Master and _Parameters_Var (if they are defined)
-    Master = getattr(Parameters_Master, VLType, None)
-    Var = getattr(Parameters_Var, VLType, None)
-
-    # ======================================================================
-    # VLType isn't in Master of Var
-    if Master==None and Var==None: return {}
-
-    # ======================================================================
-    # VLType is in Master but not in Var
-    elif Var==None: return {Master.Name : Master.__dict__}
-
-    # ======================================================================
-    # VLType is in Var
-
-    # Check all entires in Parameters_Var have the same length
-    NbNames = len(Var.Name)
-    VarNames, NewVals, errVar = [],[],[]
-    for VariableName, NewValues in Var.__dict__.items():
-        VarNames.append(VariableName)
-        NewVals.append(NewValues)
-        if len(NewValues) != NbNames:
-            errVar.append(VariableName)
-
-    if errVar:
-        attrstr = "\n".join(["{}.{}".format(VLType,i) for i in errVar])
-        message = "The following attribute(s) have a different number of entries to {0}.Name in Parameters_Var:\n"\
-        "{1}\n\nAll attributes of {0} in Parameters_Var must have the same length.".format(VLType,attrstr)
-        raise ValueError(message)
-
-    # VLType is in Master and Var
-    if Master!=None and Var !=None:
-        # Check if there are attributes defined in Var which are not in Master
-        dfattrs = set(Var.__dict__.keys()) - set(list(Master.__dict__.keys())+['Run'])
-        if dfattrs:
-            attstr = "\n".join(["{}.{}".format(VLType,i) for i in dfattrs])
-            message = "The following attribute(s) are specified in Parameters_Var but not in Parameters_Master:\n"\
-                "{}\n\nThis may lead to unexpected results.".format(attstr)
-            import warnings
-            warnings.warn(message)
-
-    # ======================================================================
-    # Create dictionary for each entry in Parameters_Var
-    VarRun = getattr(Var,'Run',[True]*NbNames) # create True list if Run not an attribute of VLType
-    ParaDict = {}
-    for Name, NewValues, Run in zip(Var.Name,zip(*NewVals),VarRun):
-        if not Run: continue
-        base = {} if Master==None else copy.deepcopy(Master.__dict__)
-        for VariableName, NewValue in zip(VarNames,NewValues):
-            base[VariableName]=NewValue
-        ParaDict[Name] = base
-
-    return ParaDict
 
 def write_image(output_file:str,vox:np.double,im_format:str=None):
     from PIL import Image, ImageOps
@@ -127,56 +65,44 @@ def rot_ax_dir(object_tilt_deg:float,object_roll_deg:float,GVXR:bool=True):
     rotation_axis_direction = rot_matrix.dot([0,0,-1])
     return rotation_axis_direction
 
-if __name__ == "__main__":
-    # Read arguments from command line
-    parser = argparse.ArgumentParser()
-    parser.add_argument("-m", "--Parameters_Master", help = "VirtualLab parameter file", required=True)
-    parser.add_argument("-v", "--Parameters_Var", help = "VirtualLab parameter file", required=True)
-    parser.add_argument("-o", "--work_dir", help = "Directory containing projections to reconstruct,"
-                        "note this is also the output directory for CIL", default=os.getcwd())
-    args = parser.parse_args()
-
-# assign directory
-    directory = args.input_file
-    if not os.path.isdir(directory):
-        raise ValueError("Input must be a valid path to a directory")
+def CT_Recon(work_dir,Name,Beam,Detector,Model,Pix_X,Pix_Y,Spacing_X,Spacing_Y,
+        rotation=[0,0,0],Headless=False, num_projections = 180,angular_step=1,
+        im_format='tiff',Nikon=None):
     
-    CILDicts = CreateParameters(args.Parameters_Master, args.Parameters_Var,'GVXR')
-    for CILName, CILParams in CILDicts.items():
-        Params = Namespace(**CILParams)
+    inputfile = f"{work_dir}/{Name}.{im_format}"
+    im = io.imread(inputfile)
 
-        inputfile = f"{args.work_dir}/{CILName}{Params.image_format}"
-        im = io.imread(inputfile)
+    if Nikon:
+        print("help")
+    
+    dist_source_center = 0-Beam[1]
+    dist_center_detector = 0+Detector[1]
 
-        beam_pos = [Params.Beam_PosX,Params.Beam_PosY,Params.Beam_PosZ]
-        det_pos = [Params.Det_PosX,Params.Det_PosY,Params.Det_PosZ]
-        dist_source_center = 0-Params.Beam_PosY
-        dist_center_detector = 0+Params.Det_PosY
+    rotation_axis_direction = rot_ax_dir(rotation[0],rotation[2])
+    # calculate geometrical magnification
+    mag = (dist_source_center + dist_center_detector) / dist_source_center
 
-        rotation_axis_direction = rot_ax_dir(Params.rotation[0],Params.rotation[2])
-        # calculate geometrical magnification
-        mag = (dist_source_center + dist_center_detector) / dist_source_center
+    ag = AcquisitionGeometry.create_Cone3D(source_position=Beam, detector_position=Detector, 
+    detector_direction_x=[1, 0, 0], detector_direction_y=[0, 0, 1],rotation_axis_position=Model,
+    rotation_axis_direction=rotation_axis_direction)  \
+    .set_panel(num_pixels=[Pix_X,Pix_Y],pixel_size=[Spacing_X/mag,Spacing_Y/mag]) \
+    .set_angles(angles=np.arange(0,num_projections,angular_step))
+        
+    if not Headless:
+        show_geometry(ag)
 
-        ag = AcquisitionGeometry.create_Cone3D(source_position=beam_pos, detector_position=det_pos, 
-        detector_direction_x=[1, 0, 0], detector_direction_y=[0, 0, 1],rotation_axis_position=[Params.Model_PosX,
-        Params.Model_PosY,Params.Model_PosZ],
-        rotation_axis_direction=rotation_axis_direction)  \
-                .set_panel(num_pixels=[Params.Pix_X,Params.Pix_Y],pixel_size=[Params.Spacing_X/mag,Params.Spacing_Y/mag]) \
-                .set_angles(angles=np.arange(0,Params.num_projections,Params.angular_step))
-        #show_geometry(ag)
-
-        im_data = AcquisitionData(array=im, geometry=ag, deep_copy=False)
-        im_data.reorder('astra')
-        im_data = TransmissionAbsorptionConverter()(im_data)
-        ig = ag.get_ImageGeometry()
-        fbp_recon = FBP(ig, ag,  device = 'gpu')(im_data)
-        recon = fbp_recon.as_array()
-        #crop_recon=recon[10:240,10:190,10:190]
-        #volume = sitk.GetImageFromArray(recon.astype('uint16'))
-        volume = sitk.GetImageFromArray(recon)
-        volume.SetSpacing([Params.Spacing_X,Params.Spacing_Y,0.5])
-        sitk.WriteImage(volume,f'{args.work_dir}/{CILName}_recon.tiff')
-
+    im_data = AcquisitionData(array=im, geometry=ag, deep_copy=False)
+    im_data.reorder('astra')
+    im_data = TransmissionAbsorptionConverter()(im_data)
+    ig = ag.get_ImageGeometry()
+    fbp_recon = FBP(ig, ag,  device = 'gpu')(im_data)
+    recon = fbp_recon.as_array()
+    #crop_recon=recon[10:240,10:190,10:190]
+    #volume = sitk.GetImageFromArray(recon.astype('uint16'))
+    volume = sitk.GetImageFromArray(recon)
+    volume.SetSpacing([Spacing_X,Spacing_Y,0.5])
+    sitk.WriteImage(volume,f'{work_dir}/{Name}_recon.tiff')
+    return
 
 
 
---
+++ Scripts/Common/VLTypes/CIL.py
@@ -0,0 +1,97 @@
+from types import SimpleNamespace as Namespace
+
+def Setup(VL, RunCIL=True):
+# if RunCIL is False or CILDicts is empty dont perform Simulation and return instead.
+    CILdicts = VL.CreateParameters(VL.Parameters_Master, VL.Parameters_Var,'GVXR')
+    if not (RunCIL or CILdicts): return
+    OUT_DIR = "{}/GVXR-Images".format(VL.PROJECT_DIR)
+    for CILName, CILParams in CILdicts.items():
+        Parameters = Namespace(**CILParams)
+
+        CILdict = {'work_dir':"{}/{}".format(OUT_DIR,CILName),
+                    'Name':CILNAME
+                }
+# Define flag to display visualisations
+        if (VL.mode=='Headless'):
+            CILdict['Headless'] = True
+        else:
+            CILdict['Headless'] = False
+
+        if hasattr(Parameters,'Nikon_file'):
+            CILdict['Nikon'] = Parameters.Nikon_file
+        else:
+            CILdict['Nikon'] = None
+
+        if hasattr(Parameters,'Beam_Pos_units'):
+            CILdict['Beam_Pos_units'] = Parameters.Beam_Pos_units
+        else:
+            CILdict['Beam_Pos_units'] = 'm'
+
+        CILdict['Beam'] = [Parameters.Beam_PosX,Parameters.Beam_PosY,
+                        Parameters.Beam_PosZ]
+
+        if hasattr(Parameters,'Detect_Pos_units'):
+            CILdict['Det_Pos_units'] = Parameters.Detect_Pos_units
+        else:
+            CILdict['Det_Pos_units'] = 'm'
+        
+        if hasattr(Parameters,'Spacing_X'): 
+            CILdict['Spacing_X'] = Parameters.Spacing_X
+        else:
+            CILdict['Spacing_X'] = 0.5
+
+        if hasattr(Parameters,'Spacing_Y'): 
+            CILdict['Spacing_Y'] = Parameters.Spacing_Y
+        else:
+            CILdict['Spacing_Y'] = 0.5
+
+        CILdict['Detector'] = [Parameters.Det_PosX,Parameters.Det_PosY,
+                    Parameters.Det_PosZ]
+
+        CILdict['Model'] = [Parameters.Model_PosX,Parameters.Model_PosY,
+                        Parameters.Model_PosZ]
+
+        CILdict['Pix_X'] = Parameters.Pix_X
+
+        CILdict['Pix_Y'] =Parameters.Pix_Y
+
+        if hasattr(Parameters,'Model_Pos_units'):
+            CILdict['Model_Pos_units'] = Parameters.Model_Pos_units    
+        else:
+            CILdict['Model_Pos_units'] = 'm'    
+        
+        if hasattr(Parameters,'rotation'):
+            CILdict['rotation'] = Parameters.rotation
+        
+        if hasattr(Parameters,'num_projections'): 
+            CILdict['num_projections'] = Parameters.num_projections
+
+        if hasattr(Parameters,'angular_step'): 
+            CILdict['angular_step'] = Parameters.angular_step
+        
+        if hasattr(Parameters,'image_format'): 
+            CILdict['im_format'] = Parameters.image_format
+
+        VL.CILData[CILName] = CILdict.copy()
+    return
+
+def Run(VL):
+    if not VL.CILData: return
+    ####################################
+    ## Test for CIL install ########
+    try:
+        from cil.framework import AcquisitionGeometry
+        print("Sucess 'CIL' is installed")
+    except ModuleNotFoundError:
+        print("module CIL is not installed are you sure you are running it in the CIl container?")
+    #########################################
+    from Scripts.Common.VLPackages.CIL.CT_reconstruction import CT_Recon
+    
+    VL.Logger('\n### Starting CIL ###\n', Print=True)
+    
+    for key in VL.CILData.keys():
+        Errorfnc = CT_Recon(**VL.CILData[key])
+        if Errorfnc:
+            VL.Exit(VLF.ErrorMessage("The following CIL routine(s) finished with errors:\n{}".format(Errorfnc)))
+
+    VL.Logger('\n### CIL Complete ###',Print=True)

dcc023563835d79b7a72ad0dba2accdadc36271e
Llion Evans <llion.evans@swansea.ac.uk>
2022-12-01 19:05:53 +0000

    Added aster_v14.6

---
+++ modules/code_aster_v14/run_tests_par.sh
@@ -0,0 +1,20 @@
+if [ -z "$ASRUN" ]; then
+    ASRUN=/home/aster/aster/bin/as_run
+fi
+
+TEST_DIR=/home/aster/shared/test
+
+mkdir -p $TEST_DIR
+cd $TEST_DIR
+$ASRUN --list --all --filter='"parallel" in testlist' --output=$TEST_DIR/testcases
+
+for testcase in `cat $TEST_DIR/testcases`
+do
+    echo "Working on $testcase..."
+    $ASRUN --test $testcase $TEST_DIR >> $TEST_DIR/screen
+done
+
+$ASRUN --diag --only_nook --astest_dir=$TEST_DIR > $TEST_DIR/diag
+
+
+/home/aster/aster/bin/as_run --list --all --filter='"parallel" in testlist'

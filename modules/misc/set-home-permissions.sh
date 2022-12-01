903bd7586efe28aa187f44be20894745a0248d7f
Llion Evans <llion.evans@swansea.ac.uk>
2022-12-01 10:28:44 +0000

    Major housekeeping of files, things likely to break.

--- modules/misc/set-home-permissions.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# User can pass e.g. --env HOST_UID=1003 so that UID in the container matches
-# with the UID on the host.
-if [ "$HOST_UID" ]; then
-    usermod -u $HOST_UID ibsim
-fi
-if [ "$HOST_GID" ]; then
-    groupmod -g $HOST_GID ibsim
-fi

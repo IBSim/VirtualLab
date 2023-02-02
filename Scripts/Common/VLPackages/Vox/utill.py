bbd31e0756d6c3b9085f9e4f84666ad1da87bd9a
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2023-02-02 12:15:58 +0000

    fixes for cad2vox to use aysmetic grids

--- Scripts/Common/VLPackages/Vox/utill.py
+++ Scripts/Common/VLPackages/Vox/utill.py
@@ -18,20 +18,32 @@ def find_the_key(dictionary, target_keys):
     return {key: dictionary[key] for key in target_keys}
 
 def check_gridsize(gridsize):
-    """check that gridsize is a non-zero positive integer"""
-    if not isinstance(gridsize, int):
-        raise TypeError("Invalid Gridsize. Must be an integer value.")
-    if gridsize < 0:
-        raise TypeError("Invalid Gridsize. Must be an integer value that is"
+    """check that gridsize is a list of 3 non-zero positive integers"""
+    if not isinstance(gridsize, list):
+        raise TypeError("Invalid Gridsize. Must be a list.")
+    if (len(gridsize) != 3):        
+            raise TypeError("Invalid Gridsize. Must be a list of three integer values.")
+
+    for i in gridsize:
+        if not isinstance(i, int):
+            raise TypeError(f"Invalid Gridsize {i}. Must be an integer value.")
+        if i < 0:
+            raise TypeError(f"Invalid Gridsize {i}. Must be an integer value that is"
                         "greater than 0.")
 
 def check_unit_length(unit_length):
-    """check that unit_length is a non-zero positive float."""
-    if not isinstance(unit_length, float):
-        raise TypeError("Invalid unit length. Must be an floating point value.")
-    if unit_length < 0:
-        raise TypeError("Invalid unit length. Must be an floating point value"
-                        " that is greater than 0.")
+    """check that unit_length is a list of three non-zero positive floats."""
+    if not isinstance(unit_length, list):
+        raise TypeError("Invalid unit_length. Must be a list.")
+    if (len(unit_length) != 3):        
+            raise TypeError("Invalid unit_length. Must be a list of three floats.")
+
+    for i in unit_length:
+        if not isinstance(i, float):
+            raise TypeError(f"Invalid unit length {i} Must be an floating point value.")
+        if i < 0:
+            raise TypeError(f"Invalid unit length {i}. Must be an floating point value"
+                            " that is greater than 0.")
 
 def check_voxinfo(unit_length,gridsize,gridmin,gridmax):
     """
@@ -48,28 +60,35 @@ def check_voxinfo(unit_length,gridsize,gridmin,gridmax):
     It then hands that off to the c++ code to calculate the
     "actual" unit_length.
 
+    Note this function converts gridsize from a list to an np array in the final step.
+
     """
-    if((gridsize==0) and (unit_length!=0.0)):
+    import numpy as np
+    #  check gridsize and unit_length are valid.
+    check_gridsize(gridsize)
+    check_unit_length(unit_length)
+
+    if((gridsize==[0,0,0]) and (0.0 not in unit_length)):
     # unit_length has been defined by user so check it is valid and
-    # then calulate gridsize.
-        check_unit_length(unit_length)
-        gridsize = int((np.max(gridmax) - np.min(gridmin))/ unit_length)
-        print("gridsize =", gridsize)
+    # then calculate gridsize.
+        for _,i in enumerate(gridsize):
+            gridsize[i] = int((gridmax[i] - gridmin[i])/ unit_length[i])
+        print("calculated gridsize =", gridsize)
 
-    elif((gridsize!=0) and (unit_length==0.0)):
-    # gridsize has been defined by user so check it is valid.
-    # Note: The calculation of unit_length is handled in c++.
-        check_gridsize(gridsize)
-
-    elif((gridsize==0) and (unit_length==0.0)):
-    #Neither has been defined
+    elif((unit_length == [0.0,0.0,0.0]) and (0 not in gridsize)):
+        pass
+    elif((gridsize==[0,0,0]) and (unit_length==[0.0,0.0,0.0])):
+        #Neither has been defined
         raise TypeError("You must define one (and only one) of either Gridsize"
                         "or unit_length")
 
     else:
-    #Both have been defined by user in which case throw an error as we need
-    # at least one of them to calculate the other.
-        raise TypeError("Both Gridsize and unit length appear to have been"
-                        " defined by the user. Please only define one.")
+        #Both have been defined by user in which case throw an error as we need
+        # at least one of them to calculate the other.
+        raise TypeError("Both Gridsize and unit length appear to have been" /
+                            " defined by the user. Please only define one.\n" /
+                            "Note: the quantity that you do define must not contain any zeros")
 
+
+    gridsize= np.array(gridsize)
     return gridsize

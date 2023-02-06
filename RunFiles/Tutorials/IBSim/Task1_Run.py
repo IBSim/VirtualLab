ef4201efe6ced4b4cae350505c438d817fb8a5ce
Rhydian Lewis <rhydian.lewis@swansea.ac.uk>
2023-02-06 14:00:46 +0000

    Added automatic download in to IBSim example and created test for it

--- RunFiles/Tutorials/IBSim/Task1_Run.py
+++ RunFiles/Tutorials/IBSim/Task1_Run.py
@@ -2,22 +2,21 @@
 #===============================================================================
 # Header
 import sys
+import os
+import requests
 sys.dont_write_bytecode=True
 from Scripts.Common.VirtualLab import VLSetup
+import VLconfig
 
-'''
-You will need to download the image-based dog bone sample from the following link
-https://ibsim.co.uk/VirtualLab/downloads/Tensile_IBSim.med
-This will need to be saved to Output/Tensile/Tutorials/Meshes
-'''
+# path to IBSim mesh file
+mesh_fname = "{}/Tensile/Tutorials/Meshes/Tensile_IBSim.med".format(VLconfig.OutputDir)
+if not os.path.isfile(mesh_fname):
+    # Download file from link
+    r = requests.get('https://ibsim.co.uk/VirtualLab/downloads/Tensile_IBSim.med')
+    # write to file
+    with open(mesh_fname,'wb') as f:
+        f.write(r.content)
 
-#===============================================================================
-# Setup
-
-Simulation='Tensile'
-Project='Tutorials'
-Parameters_Master='TrainingParameters_IBSim'
-Parameters_Var=None
 
 #===============================================================================
 # Environment

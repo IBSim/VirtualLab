f639c2082d77ec19062dedd079422b5b1f77787c
Rhydian Lewis <rhydian.lewis@swansea.ac.uk>
2023-02-06 16:04:45 +0000

    Added tests for mesh voxelisation tutorials

---
+++ tests/test_MeshVox.py
@@ -0,0 +1,39 @@
+#!/bin/python3
+import os
+import sys
+import shutil
+sys.dont_write_bytecode=True
+from subprocess import Popen
+
+VLdir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0,VLdir)
+import VLconfig
+sys.path.pop(0)
+
+Name='Mesh_Voxelisation'
+
+TutorialsDir = "{}/RunFiles/Tutorials/{}".format(VLdir,Name)
+OutputDir = '{}/VLTutorial_{}'.format(VLconfig.TEMP_DIR,Name)
+ParsedArgs = '-k Mode=T -k ShowMesh=False -k ShowRes=False -k OutputDir={}'.format(OutputDir)
+
+def test_Task1():
+    Run = Popen(['VirtualLab','-f','{}/Task1_Run.py'.format(TutorialsDir),ParsedArgs])
+    err = Run.wait()
+    assert err==0
+
+def test_Task2_pre():
+    Run = Popen(['VirtualLab','-f','{}/Task2_Pre-setup_Run.py'.format(TutorialsDir),ParsedArgs])
+    err = Run.wait()
+    assert err==0
+
+def test_Task2():
+    Run = Popen(['VirtualLab','-f','{}/Task2_Run.py'.format(TutorialsDir),ParsedArgs])
+    err = Run.wait()
+    assert err==0
+
+def test_Task3():
+    Run = Popen(['VirtualLab','-f','{}/Task3_Run.py'.format(TutorialsDir),ParsedArgs])
+    err = Run.wait()
+    assert err==0
+
+

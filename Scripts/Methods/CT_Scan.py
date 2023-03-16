b0a5d97e6c6a5308d71adc8577377f15675e2377
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2023-03-16 15:05:58 +0000

    GVXR setup should not be called if dry_run is used

--- Scripts/Methods/CT_Scan.py
+++ Scripts/Methods/CT_Scan.py
@@ -9,26 +9,51 @@ from Scripts.Common.utils import Method_base
 from Scripts.Common.VLContainer import Container_Utils as Utils
 from Scripts.VLPackages.GVXR.API import Run as CT_Scan, Dir as GVXRDir
 from Scripts.VLPackages.ContainerInfo import GetInfo
+import Scripts.Common.VLFunctions as VLF
 
 class Method(Method_base):
 
-    def Setup(self, VL, GVXRDicts, RunGVXR=True):
-        def __init__(self, VL):
-            super().__init__(VL)  # rune __init__ of Method_base
-            self.MethodName = "GVXR"
-            self.Containers_used = ["GVXR"]
+    def __init__(self, VL):
+        super().__init__(VL)  # rune __init__ of Method_base
+        self.MethodName = "GVXR"
+        self.Containers_used = ["GVXR"]
+
+    def Setup(self, VL, GVXRDicts):
         """
         GVXR - Simulation of X-ray CT scans
         """
         import json
         import glob
-        if not (self.RunFlag and GVXRDicts):
+        if not (self.RunFlag):
+            return
+        elif self.dry_run:
+    # Put some dummy values in to Data if dry_run flag is set.
+    # This is needed because, for GVXR, setup is run inside the GVXR
+    # Container. However, when dry_run is set we just want to build
+    # containers, if needed, and otherwise not actually run anything.
+    # Normally this is handled during the call to Run 
+    # (see Scripts/Common/utils.py line 17).
+
+    # Hence, this workaround is here to skip setup when doing a 
+    # dry_run but not skip the main Run method. The dry_run flag will 
+    # then work as intended.
+     
+            self.Data={'dry_run':True}
+            return
+        elif GVXRDicts=={}:
             return
         # Call setup inside a GVXR container.
         funcname = "GVXR_Setup" # function to be executed within container
         funcfile = "{}/Setup.py".format(GVXRDir) # python file where 'funcname' is located
         PROJECT_DIR_CONT = Utils.host_to_container_path(VL.PROJECT_DIR)
-        Setup = CT_Scan(funcfile, funcname, fnc_args=(GVXRDicts,PROJECT_DIR_CONT,VL.mode))
+        RC = CT_Scan(funcfile, funcname, fnc_args=(GVXRDicts,PROJECT_DIR_CONT,VL.mode))
+        if RC!=0:
+            VL.Exit(
+               VLF.ErrorMessage(
+                   "\n An error occurred in GVXR Setup:"
+               ),
+               Cleanup=False,
+            )
         Param_dir = f"{VL.PROJECT_DIR}/run_params/*.json"
         json_files = glob.glob(Param_dir)
         run_names = list(GVXRDicts.keys())
@@ -45,7 +70,6 @@ class Method(Method_base):
     
 
     def Run(self, VL):
-        import Scripts.Common.VLFunctions as VLF
         if not self.Data:
             return
         VL.Logger("\n### Starting GVXR ###\n", Print=True)

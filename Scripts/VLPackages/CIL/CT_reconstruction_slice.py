f3cc9ea7dc1a7625cc370fea78c970cccbf7e482
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2023-03-22 11:23:40 +0000

    added 2D CTRecon method

--- Scripts/VLPackages/CIL/CT_reconstruction_slice.py
+++ Scripts/VLPackages/CIL/CT_reconstruction_slice.py
@@ -29,8 +29,8 @@ def Check_GPU():
     except Exception: # this command not being found can raise quite a few different errors depending on the configuration
         raise GPUError('CIL requires an Nvidia GPU however none have been detected in system!\n' \
             'Please check your GPU is working correctly, the drivers are installed and that \n' 
-            'they are accessible inside the container.\n'
-            'Hint: You should be able to successfully run "nvidia-smi" inside the container.')
+            'they are accesible inside the container.\n'
+            'Hint: You should be able to sucessfully run "nvidia-smi" inside the container.')
 
 
 def rot_ax_dir(object_tilt_deg:float,object_roll_deg:float,GVXR:bool=True):
@@ -83,7 +83,7 @@ def crop_array(an_array,A=10):
     return an_array
     
 def CT_Recon(work_dir,Name,Beam,Detector,Model,Pix_X,Pix_Y,Spacing_X,Spacing_Y,
-        Headless=False, num_projections = 180,angular_step=1,
+        rotation=[0,0,0],Headless=False, num_projections = 180,angular_step=1,
         im_format='tiff',Nikon=None,_Name=None):
     inputfile = f"{work_dir}/{Name}"
 
@@ -94,6 +94,7 @@ def CT_Recon(work_dir,Name,Beam,Detector,Model,Pix_X,Pix_Y,Spacing_X,Spacing_Y,
         dist_source_center = 0-Beam[1]
         dist_center_detector = 0+Detector[1]
 
+        rotation_axis_direction = rot_ax_dir(rotation[0],rotation[2])
         # calculate geometrical magnification
         mag = (dist_source_center + dist_center_detector) / dist_source_center
 
@@ -103,49 +104,6 @@ def CT_Recon(work_dir,Name,Beam,Detector,Model,Pix_X,Pix_Y,Spacing_X,Spacing_Y,
         detector_direction_x=[1, 0, 0], detector_direction_y=[0, 0, 1],rotation_axis_position=Model,
         rotation_axis_direction=[0,0,1])  \
         .set_panel(num_pixels=[Pix_X,Pix_Y],pixel_size=[Spacing_X,Spacing_Y]) \
-        .set_angles(angles=angles_rad, angle_unit='radian')
-        ig = ag.get_ImageGeometry()
-    
-    #if not Headless:
-    #    breakpoint()
-    #    show_geometry(ag)
-
-    
-    im = TIFFStackReader(file_name=inputfile)
-    im_data=im.read_as_AcquisitionData(ag)
-    im_data = TransmissionAbsorptionConverter(white_level=255.0)(im_data)
-    Check_GPU()
-    
-    im_data.reorder(order='tigre')
-    fdk =  FDK(im_data)
-    recon = fdk.run()
-    recon = recon.as_array()
-    os.makedirs(f'{work_dir}/../CIL_Images', exist_ok=True)
-    #normailse data between 0 and 245
-    norm = ((recon - np.min(recon))/np.ptp(recon))*245
-    write_recon_image(f'{work_dir}/../CIL_Images/{Name}',norm);
-    return
-
-def CT_Recon_2D(work_dir,Name,Beam,Detector,Model,Pix_X,Pix_Y,Spacing_X,Spacing_Y,
-        Headless=False, num_projections = 180,angular_step=1,
-        im_format='tiff',Nikon=None,_Name=None):
-    inputfile = f"{work_dir}/{Name}"
-
-    if Nikon:
-        Nikondata = NikonDataReader(file_name=Nikon)
-        ag = Nikondata.get_geometry()
-    else:
-        dist_source_center = 0-Beam[1]
-        dist_center_detector = 0+Detector[1]
-
-        # calculate geometrical magnification
-        mag = (dist_source_center + dist_center_detector) / dist_source_center
-
-        angles_deg =np.arange(0,(num_projections+1)*angular_step,angular_step)
-        angles_rad = angles_deg *(math.pi / 180)
-        ag = AcquisitionGeometry.create_Cone2D(source_position=Beam, detector_position=Detector, 
-        detector_direction_x=[1, 0],rotation_axis_position=Model)  \
-        .set_panel(num_pixels=[Pix_X],pixel_size=[Spacing_X]) \
         .set_angles(angles=angles_rad, angle_unit='radian')
         ig = ag.get_ImageGeometry()
     

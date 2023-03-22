0d14685b3d1425eb93804888a0c72c9b5381ba73
Ben Thorpe <b.j.thorpe@swansea.ac.uk>
2023-03-22 11:29:42 +0000

    removed unnecessary functions from CT_Recon

--- Scripts/VLPackages/CIL/CT_reconstruction.py
+++ Scripts/VLPackages/CIL/CT_reconstruction.py
@@ -31,56 +31,7 @@ def Check_GPU():
             'Please check your GPU is working correctly, the drivers are installed and that \n' 
             'they are accessible inside the container.\n'
             'Hint: You should be able to successfully run "nvidia-smi" inside the container.')
-
-
-def rot_ax_dir(object_tilt_deg:float,object_roll_deg:float,GVXR:bool=True):
-    ''' function to caculate a unit vector pointing in the direction 
-    of the axis of rotation of the CAD model using a 3D rotation matrix.
-
-    Inputs:
-        object_tilt_deg - rotaion about the global x-axis (deg)
-        object_roll_deg - rotaion about the global z-axis (deg)
-    optional:
-        GVXR - flag tro skip if using GVXR since it already does this calculation
-    Return:
-        rotation_axis_direction -  unit vector pointing along the rotation axis
-    '''
-
-    import numpy as np
-    if GVXR:
-        #GVXR already acounts for tilit and roll by always rotating about the global axis
-        rotation_axis_direction = [0,0,-1]
-        return rotation_axis_direction
     
-    object_roll = object_roll_deg * np.pi /180
-    object_tilt = -object_tilt_deg * np.pi /180
-
-    tilt_matrix = np.eye(3)
-    tilt_matrix[1][1] = tilt_matrix[2][2] = np.cos(object_tilt)
-    tilt_matrix[1][2] = -np.sin(object_tilt)
-    tilt_matrix[2][1] = np.sin(object_tilt)
-
-    roll_matrix = np.eye(3)
-    roll_matrix[0][0] = roll_matrix[2][2] = np.cos(object_roll)
-    roll_matrix[0][2] = np.sin(object_roll)
-    roll_matrix[2][0] = -np.sin(object_roll)
-
-    #order of construction may be reversed, but unlikely to have both in a dataset
-    rot_matrix = np.matmul(tilt_matrix,roll_matrix)
-    rotation_axis_direction = rot_matrix.dot([0,0,-1])
-    return rotation_axis_direction
-    
-def crop_array(an_array,A=10):
-    shape=np.shape(an_array)
-    B = (shape[0]-10,shape[1]-10,shape[2]-10)
-    an_array[0:A,:,:] = 0
-    an_array[:,0:A,:] = 0
-    an_array[:,:,0:A] = 0
-    an_array[B[0]:shape[0],:,:] = 0
-    an_array[:,B[1]:shape[1],:] = 0
-    an_array[:,:,B[2]:shape[2]] = 0
-    
-    return an_array
     
 def CT_Recon(work_dir,Name,Beam,Detector,Model,Pix_X,Pix_Y,Spacing_X,Spacing_Y,
         Headless=False, num_projections = 180,angular_step=1,

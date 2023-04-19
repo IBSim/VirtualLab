a1500e963f790aeb764d17043b2393b0655a387c
Rhydian Lewis <rhydian.lewis@swansea.ac.uk>
2023-04-19 12:17:35 +0100

    Update to Salome & paravis files

--- Scripts/VLPackages/Salome/PVFunc.py
+++ Scripts/VLPackages/Salome/PVFunc.py
@@ -13,3 +13,20 @@ def Camera(FocalPoint,alpha1,alpha2,radius,ViewUp = [0,0,1]):
     renderview.CameraViewUp = ViewUp
 
     return renderview
+
+def GetDataRange(input,resname,restype):
+    if restype.lower()=='point':
+        data = input.PointData
+
+    res_available = list(data.keys())
+    if resname not in res_available:
+        print("Warning: {} not in the list of available results")
+        return
+
+    ix = res_available.index(resname)
+    range = data.GetArray(ix).GetRange()
+    return range
+
+def DataRange(input,resname):
+    return GetDataRange(input,resname,'point')
+

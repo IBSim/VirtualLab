2a220bb18824786c8f49a1fc6291ab10a347298c
Rhydian Lewis <rhydian.lewis@swansea.ac.uk>
2023-08-04 16:23:49 +0100

    Inverse work completed

--- RunFiles/Tutorials/ML/HIVE_Example/InverseSolution2.py
+++ RunFiles/Tutorials/ML/HIVE_Example/InverseSolution2.py
@@ -2,7 +2,9 @@
 '''
 This script demonstrates how the data collected in DataCollect.py
 can be used to create 3D surrogate models of the temperature and 
-Von Mises stress fields, which can be used to identify inverse solutions. 
+Von Mises stress fields, which can be used to identify inverse solutions.
+
+This script assumes that the analysis in InverseSolution.py has already been performed
 '''
 
 import sys
@@ -25,7 +27,7 @@ VirtualLab=VLSetup('HIVE','ML_analysis')
 VirtualLab.Settings(Launcher='sequential',NbJobs=1)
 
 # check data has been created, if not download
-DataFile = '{}_coil/TempNodal.hdf'.format(CoilType)
+DataFile = '{}_coil/VMNodal.hdf'.format(CoilType)
 if not VirtualLab.InProject(DataFile):
     pass # Download data from somewhere (and possibly mesh)
 
@@ -37,10 +39,10 @@ if not VirtualLab.InProject(DataFile):
 # Define GPR parameters to create model
 
 ML = Namespace()
-ML.Name = 'Temperature/GPR'
+ML.Name = 'VonMises/GPR'
 ML.File = ('GPR_Models','GPR_PCA_hdf5')
 ML.TrainingParameters = {'Epochs':1000,'lr':0.05,'Print':50}
-ML.TrainData = [DataFile, 'Features', 'Temperature',{'group':'Train'}]
+ML.TrainData = [DataFile, 'Features', 'VonMises',{'group':'Train'}]
 ML.ModelParameters = {'kernel':'Matern_2.5'} #{'min_noise':1e-8,'noise_init':1e-6}
 ML.Metric = {'nb_components':10}
 
@@ -54,11 +56,12 @@ VirtualLab.ML()
 # ====================================================================
 # analyse performance of GPR model
 DA = Namespace()
-DA.Name = 'Analysis/InverseSolution_T/GPR_{}'.format(CoilType)
-DA.File = ('InverseSolution','AnalysisT_GPR')
-DA.MLModel = 'Temperature/GPR'
+DA.Name = 'Analysis/InverseSolution_VM/GPR_{}'.format(CoilType)
+DA.File = ('InverseSolution','AnalysisVM_GPR')
+DA.MLModel_T = 'Temperature/GPR'
+DA.MLModel_VM = 'VonMises/GPR'
 DA.MeshName = 'HIVE_component' # name of the mesh used to generate the analysis (see DataCollect.py)
-DA.TestData = [DataFile, 'Features', 'Temperature',{'group':'Test'}]
+DA.TestData = [DataFile, 'Features', 'VonMises',{'group':'Test'}]
 # create comparison plots for the following indexes of the test dataset. This can be any numbers up to 300 (the size of the test dataset)
 DA.Index = [1,2]
 
@@ -76,11 +79,11 @@ VirtualLab.DA()
 # Define MLP parameters to create model
 
 ML = Namespace()
-ML.Name = 'Temperature/MLP'
+ML.Name = 'VonMises/MLP'
 ML.File = ('NN_Models','MLP_PCA_hdf5')
 ML.TrainingParameters = {'Epochs':1000,'lr':0.005,'Print':50}
-ML.TrainData = [DataFile, 'Features', 'Temperature',{'group':'Train'}]
-ML.ValidationData = [DataFile, 'Features', 'Temperature',{'group':'Test'}]
+ML.TrainData = [DataFile, 'Features', 'VonMises',{'group':'Train'}]
+ML.ValidationData = [DataFile, 'Features', 'VonMises',{'group':'Test'}]
 ML.ModelParameters = {'Architecture':[8,16,8]} 
 ML.Seed = 100 # initial weights of MLP are randomised so this ensures reproducability
 ML.Metric = {'nb_components':10}
@@ -94,11 +97,12 @@ VirtualLab.ML()
 # ====================================================================
 # analyse performance of MLP model
 DA = Namespace()
-DA.Name = 'Analysis/InverseSolution_T/MLP_{}'.format(CoilType)
-DA.File = ('InverseSolution','AnalysisT_MLP')
-DA.MLModel = 'Temperature/MLP'
+DA.Name = 'Analysis/InverseSolution_VM/MLP_{}'.format(CoilType)
+DA.File = ('InverseSolution','AnalysisVM_MLP')
+DA.MLModel_T = 'Temperature/MLP'
+DA.MLModel_VM = 'VonMises/MLP'
 DA.MeshName = 'HIVE_component' # name of the mesh used to generate the analysis (see DataCollect.py)
-DA.TestData = [DataFile, 'Features', 'Temperature',{'group':'Test'}]
+DA.TestData = [DataFile, 'Features', 'VonMises',{'group':'Test'}]
 # create comparison plots for the following indexes of the test dataset. This can be any numbers up to 300 (the size of the test dataset)
 DA.Index = [1,2]
 
--- Scripts/Experiments/HIVE/DA/ParaViS.py
+++ Scripts/Experiments/HIVE/DA/ParaViS.py
@@ -16,6 +16,12 @@ camera_default = [[0.0275,0.0225,0.0225], # camera focal point
 
 DiffTF = {**PVFunc.transferfunc_default,'ApplyPreset':['Linear Green (Gr4L)', True]}
 
+TempCB = {**PVFunc.colorbar_default,'Title':'Temperature (C)'}
+TempTF = {**PVFunc.transferfunc_default,'ApplyPreset':['RdOrYl', True]}
+
+VonMisesCB = {**PVFunc.colorbar_default,'Title':'VonMises (Pa)','ComponentTitle':'','RangeLabelFormat' : '%-#.3e'}
+VonMisesTF = {**PVFunc.transferfunc_default,'ApplyPreset':['BuGn', True]}
+
 def HeatingProfileCompare(res_file,result_names,filenames_compare,filename_diff):
     # camera settings for images
     aerial_camera= [[0.0225,0.0275,0.0225], # camera focal point
@@ -40,10 +46,9 @@ def HeatingProfileCompare(res_file,result_names,filenames_compare,filename_diff)
                               camera=aerial_camera, CB=CB, TF=DiffTF,
                               absolute_difference=True)
 
-def TemperatureCompare(res_file,result_names,filenames_compare,filename_diff):
 
-    TempCB = {**PVFunc.colorbar_default,'Title':'Temperature (C)'}
-    TempTF = {**PVFunc.transferfunc_default,'ApplyPreset':['RdOrYl', True]}
+
+def TemperatureCompare(res_file,result_names,filenames_compare,filename_diff):
 
     res_obj = PVFunc.OpenMED(res_file)
 
@@ -55,6 +60,29 @@ def TemperatureCompare(res_file,result_names,filenames_compare,filename_diff):
     PVFunc.Difference(res_obj,result_names,filename_diff,
                               camera=camera_default, CB=CB, TF=DiffTF,
                               absolute_difference=True)
+
+def VonMisesCompare(res_file,result_names,filenames_compare,filename_diff):
+
+    res_obj = PVFunc.OpenMED(res_file)
+
+    # Create plot of the ML model and simulation result with the same colour bar (which is taken from the data at index 1)
+    PVFunc.Compare(res_obj,result_names,filenames_compare,
+                   camera=camera_default,CB=VonMisesCB,TF=VonMisesTF,compare_ix=1)
+
+    CB = {**VonMisesCB,'Title':'\u0394 {}'.format(VonMisesCB['Title'])}
+    PVFunc.Difference(res_obj,result_names,filename_diff,
+                              camera=camera_default, CB=CB, TF=DiffTF,
+                              absolute_difference=True)
+
+
+def CaptureTemperature(res_file,result_names,filenames):
+    res_obj = PVFunc.OpenMED(res_file)
+    PVFunc.ImageCapture(res_obj,result_names,filenames,CB=TempCB,TF=TempTF)
+
+def CaptureVonMises(res_file,result_names,filenames):
+    res_obj = PVFunc.OpenMED(res_file)
+    PVFunc.ImageCapture(res_obj,result_names,filenames,CB=VonMisesCB,TF=VonMisesTF)
+
 
 if __name__=='__main__':
     EvalList = PVFunc.GetEvalInfo()

0d5dfab6fe88fcf4c023a84d88fb5bf745a91657
Llion Marc Evans <1497742-llionevans@users.noreply.gitlab.com>
2023-09-12 17:08:33 +0000

    Google analytics finally fixed (needs tweaks)

--- Scripts/Common/Analytics.py
+++ Scripts/Common/Analytics.py
@@ -21,8 +21,7 @@ done in VLconfig.py.
 '''
 
 def Run(Category,Action,Label):
-    #tracking_id = 'UA-112907949-3' #tid
-    tracking_id = 'G-M4X1J02VS1' #tid
+    tracking_id = 'UA-112907949-3' #tid
     clientid_str = str(datetime.now()) #cid
     campaign_name_str = 'UbuntuVM' #cn
     key1 = 'UbuntuVM' #key1
@@ -44,3 +43,42 @@ def Run(Category,Action,Label):
     # https://www.optimizesmart.com/understanding-universal-analytics-measurement-protocol/
     # https://requests.readthedocs.io/en/master/user/quickstart/
     # https://developers.google.com/analytics/devguides/collection/protocol/v1/reference
+
+'''
+# The following is the new method for google analytics.
+# GA4 replaces UA which has been discontinued.
+# Currently, the variables in the script below have been hard-coded.
+# These need amending to match the behaviour of the script above.
+
+# Variables to be amended:
+# client_id, timestamp_micros, name, items{"name_1":"string_name1"}, params
+# items are embedded within params. Use the guided event builder (link below)
+# to figure best way to organise these
+
+import requests
+import json
+url = "https://www.google-analytics.com/mp/collect?measurement_id=G-M4X1J02VS1&api_secret=XwWir8mZRzees0BOKtHFmg"
+payload = {
+  "client_id": "manual",
+  "timestamp_micros":"1694535623258000",
+  "non_personalized_ads": True,
+  "events": [
+    {
+      "name": "Label",
+      "params": {
+        "items": [
+            {"name_1":"string_name1"}
+        ],
+        "key1": "UbuntuVM"
+      }
+    }
+  ]
+}
+r = requests.post(url,data=json.dumps(payload),verify=True)
+print(r.status_code)
+
+# Useful urls
+# https://codelabs.developers.google.com/codelabs/GA4_MP#0 # Tutorial for getting this to work
+# https://ga-dev-tools.google/ga4/event-builder/ # Guided event builder
+# https://colab.research.google.com/ # Online tool for testing
+'''
